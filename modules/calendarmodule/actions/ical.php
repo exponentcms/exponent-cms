b4c3a70afcaad6b5c8a3b9a504b12c5659092756
dleffler <dleffler@hughes.net>
2011-03-01 13:49:14 -0500

    Fixes ical calendar module aciton for php version under 5.3

--- modules/calendarmodule/actions/ical.php
+++ modules/calendarmodule/actions/ical.php
@@ -70,22 +70,94 @@ if (isset($_GET['date_id']) || isset($_GET['src'])) {
 			$Filename = preg_replace('/\s+/','',$title);  // without whitespace
 		}	
 
-		$search = array ('/"/',
-						 '/,/',
-						 '/\n/',
-						 '/\r/',
-						 '/:/',
-						 '/;/',
-						 '/\\//');                    // evaluate as php
+		if (!function_exists("quoted_printable_encode")) {
+			function quoted_printable_encode($input, $line_max = 75) { 
+			   $hex = array('0','1','2','3','4','5','6','7', 
+									  '8','9','A','B','C','D','E','F'); 
+			   $lines = preg_split("/(?:\r\n|\r|\n)/", $input); 
+			   $linebreak = "=0D=0A=\r\n"; 
+			   /* the linebreak also counts as characters in the mime_qp_long_line 
+				* rule of spam-assassin */ 
+			   $line_max = $line_max - strlen($linebreak); 
+			   $escape = "="; 
+			   $output = ""; 
+			   $cur_conv_line = ""; 
+			   $length = 0; 
+			   $whitespace_pos = 0; 
+			   $addtl_chars = 0; 
 
-		$replace = array ('\"',
-						 '\\,',
-						 '\\n',
-						 '',
-						 '\:',
-						 '\\;',
-						 '\\\\');
+			   // iterate lines 
+			   for ($j=0; $j<count($lines); $j++) { 
+				 $line = $lines[$j]; 
+				 $linlen = strlen($line); 
 
+				 // iterate chars 
+				 for ($i = 0; $i < $linlen; $i++) { 
+				   $c = substr($line, $i, 1); 
+				   $dec = ord($c); 
+
+				   $length++; 
+
+				   if ($dec == 32) { 
+					  // space occurring at end of line, need to encode 
+					  if (($i == ($linlen - 1))) { 
+						 $c = "=20"; 
+						 $length += 2; 
+					  } 
+
+					  $addtl_chars = 0; 
+					  $whitespace_pos = $i; 
+				   } elseif ( ($dec == 61) || ($dec < 32 ) || ($dec > 126) ) { 
+					  $h2 = floor($dec/16); $h1 = floor($dec%16); 
+					  $c = $escape . $hex["$h2"] . $hex["$h1"]; 
+					  $length += 2; 
+					  $addtl_chars += 2; 
+				   } 
+
+				   // length for wordwrap exceeded, get a newline into the text 
+				   if ($length >= $line_max) { 
+					 $cur_conv_line .= $c; 
+
+					 // read only up to the whitespace for the current line 
+					 $whitesp_diff = $i - $whitespace_pos + $addtl_chars; 
+
+					/* the text after the whitespace will have to be read 
+					 * again ( + any additional characters that came into 
+					 * existence as a result of the encoding process after the whitespace) 
+					 * 
+					 * Also, do not start at 0, if there was *no* whitespace in 
+					 * the whole line */ 
+					 if (($i + $addtl_chars) > $whitesp_diff) { 
+						$output .= substr($cur_conv_line, 0, (strlen($cur_conv_line) - 
+									   $whitesp_diff)) . $linebreak; 
+						$i =  $i - $whitesp_diff + $addtl_chars; 
+					  } else { 
+						$output .= $cur_conv_line . $linebreak; 
+					  } 
+
+					$cur_conv_line = ""; 
+					$length = 0; 
+					$whitespace_pos = 0; 
+				  } else { 
+					// length for wordwrap not reached, continue reading 
+					$cur_conv_line .= $c; 
+				  } 
+				} // end of for 
+
+				$length = 0; 
+				$whitespace_pos = 0; 
+				$output .= $cur_conv_line; 
+				$cur_conv_line = ""; 
+
+				if ($j<=count($lines)-1) { 
+				  $output .= $linebreak; 
+				} 
+			  } // end for 
+
+			  return trim($output); 
+			} // end quoted_printable_encode 
+		}
+		
 	//	$tz = "America/New_York";
 		$tz = DISPLAY_DEFAULT_TIMEZONE;
 		$msg = "BEGIN:VCALENDAR\n";
@@ -132,7 +204,6 @@ if (isset($_GET['date_id']) || isset($_GET['src'])) {
 		//		$body = str_replace(array("\n"), "=0D=0A", $body);
 
 				// $body = chop(strip_tags(str_replace(array("<br />","<br>","br/>"),"\r",$items[$i]->body)));
-				// $body = preg_replace($search, $replace, $body);
 				// $body = wordwrap($body);
 				// $body = str_replace("\n","\n  ",$body);
 
