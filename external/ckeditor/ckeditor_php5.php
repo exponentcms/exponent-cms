9c3d5c9424b5515e7dc3ae93f03ec53603d9b0b9
Phillip Ball <phillip@phillipball.com>
2011-05-13 11:06:24 -0700

    upgrade CKEditor. This fixes some issues where Dialogues won't close.

--- external/ckeditor/ckeditor_php5.php
+++ external/ckeditor/ckeditor_php5.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '3.5.3';
+	const version = '3.6';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'B37D54V';
+	const timestamp = 'B49E5BQ';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "B37D54V";
+	public $timestamp = "B49E5BQ";
 	/**
 	 * An array that holds event listeners.
 	 */
