b05db05617c05a2ce72a65f9ee3a648ad8428458
Phillip Ball <phillip@phillipball.com>
2011-06-24 10:33:20 -0700

    js folder no longer needed

--- js/ImageManagerHTMLArea.js
+++
@@ -1,9 +0,0 @@
-function setContent(imagePath,relative) {
-	if ((imagePath != null) && (imagePath != "")) {
-		opener.window.document.getElementById('f_url').focus()
-		opener.window.document.getElementById('f_url').value = imagePath;
-		opener.window.relativeExponent = relative;
-		opener.onPreview();
-	}
-	window.close();
-}
