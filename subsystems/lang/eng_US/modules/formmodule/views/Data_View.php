e1f0ee4dbca50821df0f35c6e59a786298eecce9
dleffler <dleffler@hughes.net>
2011-07-29 21:35:51 -0400

    Fixes problem w/ new form module Data view which had missing link words due to a misnamed language file

--- subsystems/lang/eng_US/modules/formmodule/views/Data_View.php
+++ subsystems/lang/eng_US/modules/formmodule/views/Data_View.php
@@ -1,10 +1,6 @@
 <?php
 
 return array(
-	'alt_userperm'=>'Assign permissions on this Module',
-	'alt_groupperm'=>'Assign group permissions on this Module',
-	'alt_configure'=>'Configure this Module',
-	
 	'enter_data'=>'Enter Data',
 	'export_cvs'=>'Export CVS',
 	'edit_settings'=>'Form Settings',
