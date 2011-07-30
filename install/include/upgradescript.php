c49d1d17c06a098bf05a7769ba0eaf6b6c4ed10e
dleffler <dleffler@hughes.net>
2011-07-29 21:16:42 -0400

    Updates the format and organization of the api documentation.  This is a work in progress. [#199]

--- install/include/upgradescript.php
+++ install/include/upgradescript.php
@@ -1,26 +1,29 @@
 <?php
+
+##################################################
+#
+# Copyright (c) 2004-2011 OIC Group, Inc.
+# Written and Designed by James Hunt
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
 /**
- *  This file is part of Exponent
- *  Exponent is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU
- *  General Public License as published by the Free
- *  Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
- *
- * The file that holds the upgradescript class
- *
- * @link http://www.gnu.org/licenses/gpl.txt GPL http://www.gnu.org/licenses/gpl.txt
- * @package Exponent-CMS
- * @copyright 2004-2011 OIC Group, Inc.
- * @author Adam Kessler <adam@oicgroup.net>
- * @version 2.0.0
+ * @package Installation
+ * @subpackage Upgrade
  */
 
 /**
  * This is the class upgradescript
- *
- * @subpackage Upgrade
- * @package Installation
  */
 class upgradescript {
 	protected $from_version = '9999.99.99'; //set this default ridiculously high so that it NEVER runs
--- plugins/function.simplenote.php
+++ plugins/function.simplenote.php
@@ -1,28 +1,32 @@
 <?php
+
+##################################################
+#
+# Copyright (c) 2004-2011 OIC Group, Inc.
+# Written and Designed by James Hunt
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
 /**
- * This smarty function is for used to include the SimpleNote Attachable item
- * 
- * This file is part of Exponent Content Management System
- *
- * Exponent is free software; you can redistribute
- * it and/or modify it under the terms of the GNU
- * General Public License as published by the Free
- * Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * @category   Exponent CMS
- * @package    Plugins
- * @author     Jonathan Worent <jonathan@oicgroup.net>
- * @copyright  2004-2011 OIC Group, Inc.
- * @license    GPL: http://www.gnu.org/licenses/gpl.txt
- * @version    Release: @package_version@
- * @link       http://www.exponent-docs.org/api/package/PackageName
+ * Smarty plugin
+ * @package Smarty-Plugins
+ * @subpackage Function
  */
 
 /**
- * Exponent Smarty {simplenote} function plugin 
+ * Smarty {simplenote} function plugin 
  * 
- * Type:     block function<br>
+ * Type:     function<br>
  * Name:     simplenote<br>
  * Purpose:  Include the SimpleNote Attachable Item on the page.<br>
  * 
@@ -50,7 +54,7 @@ function smarty_function_simplenote($params,&$smarty) {
             'content_id'=>$params['content_id'], 
             'content_type'=>$params['content_type'], 
             'hideform'=>$hideform, 
-            'hidecomments'=>$hidecomments,
+            'hidecomments'=>$hidenotes,
             'title'=>$title,
             'formtitle'=>$formtitle,
             'require_login'=>$require_login,
