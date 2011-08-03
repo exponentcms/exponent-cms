ced9081d25af2736f01e6c9e5686077be01b4b97
dleffler <dleffler@hughes.net>
2011-08-02 20:59:09 -0400

    db_recover.php is no longer needed (but had language files tied in until now)

---
+++ framework/core/subsystems-1/lang/eng_US/modules/administrationmodule/actions/config_save.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'bad_prefix'=>'Invalid table prefix.  The table prefix can only contain alphanumeric characters.',
+	'cant_connect'=>'Unable to connect to database server.  Make sure that the database specified exists, and the user account specified has access to the server.',
+	'perm_denied'=>'Unable to run %s commands.',
+	'create_admin'=>'Creating default administrator account.',
+	'activate_panel'=>'Activating Administration Module by default.',
+	'home'=>'Home',
+	'create_section'=>'Creating default "Home" section.',
+	'disabled'=>'The Database Recovery Script is turned off by default, for security reasons.  To enable it, please edit this PHP file, and uncomment the exit() line.',
+);
+
+?>
