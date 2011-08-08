a2ccafeb1f03b0e4d942151f94d4e86c08a1ad93
dleffler <dleffler@hughes.net>
2011-08-08 18:05:00 -0400

    deletes (old school) preview module and also the old pixidou temp folder inside that module [#292 state:resolved]

--- framework/core/subsystems-1/lang/eng_US/modules/previewmodule/class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-	'module_name'=>'Preview Link',
-	'module_description'=>'Allows logged-in users to preview the site as an anonymous user, and switch back.  Mostly useful for editors and administrators.',
-);
-
-?>
