5a3be29a7391310914dbc9641537ddfa00d2caf9
rasseljandavid <rasseljandavid@gmail.com>
2011-08-22 19:35:10 +0800

    Added FK to product table and created a relationship in the storeCategory model

--- framework/core/definitions/product.php
+++ framework/core/definitions/product.php
@@ -55,6 +55,10 @@ return array(
         DB_FIELD_TYPE=>DB_DEF_STRING,
         DB_FIELD_LEN=>100,
         DB_INDEX=>10), 
+	"shopzilla_product_type"=>array(
+        DB_FIELD_TYPE=>DB_DEF_STRING,
+        DB_FIELD_LEN=>100,
+        DB_INDEX=>10), 
 	'sef_url'=>array(
 		DB_FIELD_TYPE=>DB_DEF_STRING,
 		DB_FIELD_LEN=>150,
