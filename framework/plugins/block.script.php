869a9146abdd50fd89a2c6a18d94b4b6a397d2d8
Phillip Ball <phillip@phillipball.com>
2011-09-13 18:26:18 -0700

    some helper functionality to help make yui2in3 backwards compatible

--- framework/plugins/block.script.php
+++ framework/plugins/block.script.php
@@ -23,9 +23,32 @@ function smarty_block_script($params,$content,&$smarty, &$repeat) {
 		
 		if (empty($params['unique'])) die("<strong style='color:red'>The 'unique' parameter is required for the {script} pluggin.</strong>"); 
 		
+        if ((isset($params['yui2mods']) || isset($params['yuimodules'])) && !strstr($content,"YUI(")) {
+            $params['yui3mods'] = 1;
+            $yui2mods = $params['yui2mods']?$params['yui2mods']:$params['yuimodules'];
+            $toreplace = array('"',"'"," ");
+            $stripmodquotes = str_replace($toreplace, "", $yui2mods);               
+            $splitmods = explode(",",$stripmodquotes);
+
+            $y3wrap = "YUI(EXPONENT.YUI3_CONFIG).use(";            
+            $y3wrap .= "'yui2-yahoo-dom-event', ";
+            foreach ($splitmods as $key=>$mod) {
+                if ($mod=="menu") {
+                    $y3wrap .= "'yui2-container', ";
+                }
+                $y3wrap .= "'yui2-".$mod."', ";
+            }
+            $y3wrap .= "function(Y) {\r\n";
+            $y3wrap .= "var YAHOO=Y.YUI2;";
+            $y3wrap .= $content;
+            $y3wrap .= "});";
+            
+            $content = $y3wrap;
+        }
+		
         expJavascript::pushToFoot(array(
             "unique"=>$params['unique'],
-            "yui2mods"=>$params['yui2mods']?$params['yui2mods']:$params['yuimodules'],
+            //"yui2mods"=>$params['yui2mods']?$params['yui2mods']:$params['yuimodules'],
             "yui3mods"=>$params['yui3mods'],
             "content"=>$content,
             "src"=>$params['src']
