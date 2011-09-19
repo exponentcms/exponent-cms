b4fa228013c092689cb7b3f92b67a4442667cc32
Jonathan Worent <jonathan@inspiredbytes.net>
2011-09-19 12:18:02 -0500

    BUG FIX - In the Twitter Module Confing Selecting Mentions pulled Public and Selecting Public pulled Mentions.
    The Switch statement in the controller now matched the confing. 0-user, 1-home, 2-friends, 3-mentions, 4-public

--- framework/modules/twitter/controllers/twitterController.php
+++ framework/modules/twitter/controllers/twitterController.php
@@ -55,12 +55,12 @@ class twitterController extends expController {
 					$tweets = $twitter->statusesFriendsTimeline(null,null,$this->config['twlimit']);
 			        break;
 		        case 3:
-					// get public timeline
-					$tweets = $twitter->statusesPublicTimeline();
-			        break;
-		        case 4:
 					// get mentions
 					$tweets = $twitter->statusesMentions(null,null,$this->config['twlimit']);
+			        break;
+		        case 4:
+					// get public timeline
+					$tweets = $twitter->statusesPublicTimeline();
 			        break;
 		        default:
 			        // get users timeline
