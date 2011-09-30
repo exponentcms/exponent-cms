a96765ef52387a62489fb7f1e7793293fdd05e9c
dleffler <dleffler@hughes.net>
2011-09-30 11:55:48 -0400

    Updates version, RELEASE, etc for next version 2.0.2

--- exponent_version.php
+++ exponent_version.php
@@ -19,39 +19,39 @@
 
 if (!defined('EXPONENT_VERSION_MAJOR')) {
 	// the RELEASE constant is changed by the build scripts at code freeze
-	define('RELEASE','RELEASE');
+	define('RELEASE','%%RELEASE%%');
 	if (RELEASE != '%%RELEASE%%') {
 
 		/* exdoc
 		 * This is the major version number of Exponent; the 1 in 1.96.2-beta3
 		 */
-		define('EXPONENT_VERSION_MAJOR','2');
+		define('EXPONENT_VERSION_MAJOR','%%MAJOR%%');
 		/* exdoc
 		 * This is the minor version number of Exponent; the 96 in 1.96.2-beta3
 		 */
-		define('EXPONENT_VERSION_MINOR','0');
+		define('EXPONENT_VERSION_MINOR','%%MINOR%%');
 		/* exdoc
 		 * This is the revision version number of Exponent; the 2 in 1.96.2-beta3
 		 */
-		define('EXPONENT_VERSION_REVISION','1');
+		define('EXPONENT_VERSION_REVISION','%%REVISION%%');
 		/* exdoc
 		 * This specifies the type of release, either 'alpha','beta','release-candidate' or '' (for stable).
 		 */
-		define('EXPONENT_VERSION_TYPE','');
+		define('EXPONENT_VERSION_TYPE','%%TYPE%%');
 		/* exdoc
 		 * This number is bumped each time a distribution of a single version is
 		 * released.  For instance, the 3rd beta has an version type iteration of 3.
 		 */
-		define('EXPONENT_VERSION_ITERATION',''); // only applies to betas, alphas, or release candidates
+		define('EXPONENT_VERSION_ITERATION','%%ITERATION%%'); // only applies to betas, alphas, or release candidates
 		/* exdoc
 		 * This is the date that this version of Exponent was released.
 		 */
-		define('EXPONENT_VERSION_BUILDDATE','1317396444');
+		define('EXPONENT_VERSION_BUILDDATE','%%BUILDDATE%%');
 	} else {
 		// the info for the "next" version if we are a pre-release from the repository
 		define('EXPONENT_VERSION_MAJOR','2');
 		define('EXPONENT_VERSION_MINOR','0');
-		define('EXPONENT_VERSION_REVISION','1');
+		define('EXPONENT_VERSION_REVISION','2');
 		define('EXPONENT_VERSION_TYPE','develop');
 		define('EXPONENT_VERSION_ITERATION','');
 		define('EXPONENT_VERSION_BUILDDATE',time());
