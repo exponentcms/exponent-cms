5ac36ccae6540807349f7b170d48c2845aeaceda
dleffler <dleffler@hughes.net>
2011-10-18 10:45:25 -0400

    Additional (new) files with pixidou upgrade to using upload.class.php v0.31 (from 0.25)

---
+++ framework/modules/pixidou/download.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 +------------------------------------------------------------------------+
+	 | Pixidou - an Open Source AJAX Image Editor                      		  |
+	 +------------------------------------------------------------------------+
+	 | download.php	                                                          |
+	 +------------------------------------------------------------------------+
+	 | Copyright (c) Asvin Balloo 2008. All rights reserved. 	              |
+	 | Version       0.1                                                      |
+	 | Last modified 29/10/2008                                               |
+	 | Email         asvin.balloo@gmail.com                                   |
+	 | Web           http://htmlblog.net                                      |
+	 +------------------------------------------------------------------------+
+	 | This program is free software; you can redistribute it and/or modify   |
+	 | it under the terms of the GNU General Public License version 2 as      |
+	 | published by the Free Software Foundation.                             |
+	 |                                                                        |
+	 | This program is distributed in the hope that it will be useful,        |
+	 | but WITHOUT ANY WARRANTY; without even the implied warranty of         |
+	 | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          |
+	 | GNU General Public License for more details.                           |
+	 |                                                                        |
+	 | You should have received a copy of the GNU General Public License      |
+	 | along with this program; if not, write to the                          |
+	 |   Free Software Foundation, Inc., 59 Temple Place, Suite 330,          |
+	 |   Boston, MA 02111-1307 USA                                            |
+	 |                                                                        |
+	 +------------------------------------------------------------------------+
+	 */
+
+	require_once('../../../exponent.php');
+
+	header("Content-type: application/octet-stream");
+	header('Content-Disposition: attachment; filename="'.$_GET['file'].'"');
+//	readfile('images/'.$_GET['file']);
+	readfile(BASE.'tmp/pixidou/'.$_GET['file']);
+?>
