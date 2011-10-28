782759504630a2d9bba4dc56e6570154370748e0
dleffler <dleffler@hughes.net>
2011-10-28 13:38:54 -0400

    Finalizing release v2.0.2

--- exponent_version.php
+++ exponent_version.php
@@ -46,7 +46,7 @@ if (!defined('EXPONENT_VERSION_MAJOR')) {
 		/* exdoc
 		 * This is the date that this version of Exponent was released.
 		 */
-		define('EXPONENT_VERSION_BUILDDATE','1319823316');
+		define('EXPONENT_VERSION_BUILDDATE','1319823534');
 	} else {
 		// the info for the "next" version if we are a pre-release from the repository
 		define('EXPONENT_VERSION_MAJOR','2');
