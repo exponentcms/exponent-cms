8dfa3779bda180d7005401e1a206b1a903bc3fcc
dleffler <dleffler@hughes.net>
2011-11-01 17:07:51 -0400

    Adds Spanish language file shell using language name

---
+++ framework/core/lang/Espa�ol.info.php
@@ -0,0 +1,28 @@
+<?php
+
+##################################################
+#
+# Copyright (c) 2004-2011 OIC Group, Inc.
+# Written and Designed by James Hunt
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
+return array(
+	'name'=>'Español',
+	'charset'=>'UTF-8', // Used in the META.  Very important.
+	'author'=>'OIC Group, Inc.',
+	'locale'=>'es', // See RFC 1766 and ISO 639-2
+	'default_view'=>'Default' // For recursion in view resolution
+);
+
+?>
---
+++ framework/core/lang/Espa�ol.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+	"Add Module"=>"Añadir el Módulo",
+	"Add text at the top"=>"Añadir texto en la parte superior",
+);
+?>
