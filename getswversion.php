12a81dda0896ddc865dbcc1dd732c214876954c3
dleffler <dleffler@hughes.net>
2011-12-23 23:03:01 -0500

    Revises install/upgrade process to no longer be semi-automated.  Now installation MUST be called explicitly and if an 'upgrade' condition exists, but the site is still configured, the admin user will be notified with a link to upgrade the site after logging in.  Also contains the basics for doing an online version check displaying a notice once per session.  Still need a location (or new method) to store the version check. [#421 #314]

---
+++ getswversion.php
@@ -0,0 +1,32 @@
+<?php
+
+##################################################
+#
+# Copyright (c) 2004-2011 OIC Group, Inc.
+# Written and Designed by James Hunt
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
+// Initialize the Exponent Framework
+require_once('exponent.php');
+
+$swversion->major = EXPONENT_VERSION_MAJOR;
+$swversion->minor = EXPONENT_VERSION_MINOR;
+$swversion->revision = EXPONENT_VERSION_REVISION+1;
+$swversion->type = EXPONENT_VERSION_TYPE;
+$swversion->iteration = EXPONENT_VERSION_ITERATION;
+$swversion->builddate = EXPONENT_VERSION_BUILDDATE;
+
+echo expJavascript::ajaxReply(201, '', $swversion);
+
+?>
