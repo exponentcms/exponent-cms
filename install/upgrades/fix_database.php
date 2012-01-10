421071499282ffb510405afd57525869f0a3603d
dleffler <dleffler@hughes.net>
2012-01-10 18:05:04 -0500

    Adds new upgrade script to fix bad paths in user profile extensions and default avatar.  Also updates the fix_database upgrade script to only run when upgrading from OLD versions. [#456 #458 state:resolved]

--- install/upgrades/fix_database.php
+++ install/upgrades/fix_database.php
@@ -27,7 +27,7 @@
  */
 class fix_database extends upgradescript {
 	protected $from_version = '0.96.3';
-//	protected $to_version = '2.0.1';
+	protected $to_version = '2.0.1';  // we no longer need to do this for every upgrade, only from OLD ones
 
 	/**
 	 * name/title of upgrade script
