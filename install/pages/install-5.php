fae06920c21c0896bfa92dbb3e60f5c5aa571389
dleffler <dleffler@hughes.net>
2012-01-20 19:03:52 -0500

    Places standard installation sample in install folder instead of multiple copies across each theme folder.  Should reduce package size by over 7 megs.  [#311 ]

--- install/pages/install-5.php
+++ install/pages/install-5.php
@@ -30,6 +30,10 @@ echo gt('Pick Your Theme');
 
 $themes = array();
 if (is_readable(BASE . 'themes')) {
+    echo '<form method="post" action="index.php">';
+    echo '<div class="theme">';
+    echo file_exists(BASE . "install/samples/sample.eql") ? '<b font-size:120%><input type="checkbox" name="install_sample" value="install/samples/sample" class="checkbox"><label class="label ">'.gt('Install Sample Content?').'</label></b>' : '';
+    echo "</div>";
     $dh = opendir(BASE . 'themes');
     while (($file = readdir($dh)) !== false) {
         if (is_readable(BASE . "themes/$file/class.php")) {
@@ -40,21 +44,16 @@ if (is_readable(BASE . 'themes')) {
              * @name $theme
              */
             $theme          = new $file();
-            
-            echo '<div class="theme clearfix">
-            <form method="post" action="index.php">
-            ';
+            echo '<div class="theme clearfix">';
             echo is_readable(BASE . "themes/$file/preview.jpg") ? "<img src=\"".URL_FULL."thumb.php?src=themes/$file/preview.jpg&amp;w=100&amp;q=75\" class=\"themepreview\">" : "";
             echo "<h2>".$theme->name()."</h2>";
             echo "<em>".$theme->author()."</em>";
-            echo "<p>".$theme->description().'<br /><br />
-                <input type="hidden" name="page" value="install-6">
-                <input type="hidden" name="sc[DISPLAY_THEME_REAL]" value="'.$file.'" id="sc[DISPLAY_THEME_REAL]">';
-			echo file_exists(BASE . "themes/$file/sample.eql") ? '<input type="checkbox" name="install_sample" value="1" class="checkbox"><label class="label ">'.gt('Install Sample Content').';</label><br /><br />' : '';
-            echo '<button class="awesome green small">'. gt('Use') .' '.$theme->name().'</button>'.
-	            '</p>';
-            echo "</form></div>";
+            echo "<p>".$theme->description().'</p>
+                  <input type="hidden" name="page" value="install-6">';
+            echo '<button class="awesome green small" name="sc[DISPLAY_THEME_REAL]" value="'.$file.'" id="sc[DISPLAY_THEME_REAL]" style="float:right">'. gt('Use') .' '.$theme->name().'</button>';
+            echo "</div>";
         }
     }
+    echo "</form>";
 }
 ?>
