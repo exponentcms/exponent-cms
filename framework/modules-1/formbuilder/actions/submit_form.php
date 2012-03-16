e95a35bce834b63483079a1fe1a917de44cf70fb
dleffler <dleffler@hughes.net>
2012-03-16 08:00:21 -0400

    Fixes to submit form email validation

--- framework/modules-1/formbuilder/actions/submit_form.php
+++ framework/modules-1/formbuilder/actions/submit_form.php
@@ -51,7 +51,7 @@ foreach ($controls as $c) {
 //        $emailFields[$c->name] = call_user_func(array($control_type,'templateFormat'),$emailValue,$ctl);
         $emailFields[$c->name] = call_user_func(array($control_type,'templateFormat'),$value,$ctl);
         $captions[$c->name] = $c->caption;
-		if ($c->name == "email" && expValidator::is_valid_email(array("email" => $value), "email", array()) === true) {
+		if ($c->name == "email" && expValidator::isValidEmail($value)) {
 			$from = $value;
 		}
 		if ($c->name == "name") {
