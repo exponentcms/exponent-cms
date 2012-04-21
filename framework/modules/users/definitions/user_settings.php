2f13d7dfda0ce32f78b125ba9aef15f9f6540d04
dleffler <dleffler@hughes.net>
2012-04-21 11:17:34 -0400

    The user profile extension tables have the auto-increment flag set for the user_id column which shouldn't be since it is simply a pointer to the real user_id in the user table.  Not sure if this will fix this issue though. [#579]

--- framework/modules/users/definitions/user_settings.php
+++ framework/modules/users/definitions/user_settings.php
@@ -23,8 +23,7 @@
 return array(
 	'user_id'=>array(
 		DB_FIELD_TYPE=>DB_DEF_ID,
-		DB_PRIMARY=>true,
-		DB_INCREMENT=>true),
+		DB_PRIMARY=>true),
 	'USER_WYSIWYG_EDITOR'=>array(
 		DB_FIELD_TYPE=>DB_DEF_STRING,
 		DB_FIELD_LEN=>20),
