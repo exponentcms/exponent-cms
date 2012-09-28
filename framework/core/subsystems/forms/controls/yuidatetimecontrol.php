612c5d021a8597ad30132742d3814921fc5c0185
dleffler <dleffler@hughes.net>
2012-09-28 06:49:18 -0400

    Hack to fix event problem w/ yuidatetimecontrol where clicking the checkbox doesn't get the event to show/hide the caleandar.

--- framework/core/subsystems/forms/controls/yuidatetimecontrol.php
+++ framework/core/subsystems/forms/controls/yuidatetimecontrol.php
@@ -81,7 +81,7 @@ class yuidatetimecontrol extends formcontrol {
             $html .= '<input id="pub-'.$name.'" type="checkbox" name="'.$name.'"';
             $html .= $this->checked ? ' checked>'.$this->edit_text : '>'.$this->edit_text;
             $html .= '<div ';
-            $html .= $this->checked ? 'style="display:none"': 'style="display:block"';
+//            $html .= $this->checked ? 'style="display:none"': 'style="display:block"';
             $html .= ' id="datetime-'.$name.'">';
             $html .= ($this->showdate) ? $datectl->controlToHTML($name."date") : "";
             $html .= '<div class="yuitime">';
@@ -104,7 +104,7 @@ class yuidatetimecontrol extends formcontrol {
         ";
         
         expJavascript::pushToFoot(array(
-            "unique"=>"zzdatetime-".$name,
+            "unique"=>"zdatetime-".$name,
             "yui3mods"=>"1",
             "content"=>$script,
          ));
