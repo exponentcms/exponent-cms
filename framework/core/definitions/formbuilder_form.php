802e3152b12de60f0f83bf202a2eeeb2544df998
dleffler <dleffler@hughes.net>
2013-01-09 21:24:21 -0500

    Fixes inability to create forms (all links in a newly added form module go to a 'page not found' error), but only if the site was a full fresh install of v2.1.0 (upgrades and previous versions were not affected) [#853 state:resolved]

--- framework/core/definitions/formbuilder_form.php
+++ framework/core/definitions/formbuilder_form.php
@@ -40,7 +40,7 @@ return array(
 		DB_FIELD_LEN=>100),
 	'is_email'=>array(
 		DB_FIELD_TYPE=>DB_DEF_BOOLEAN),
-    'is_email'=>array(
+    'is_saved'=>array(
   		DB_FIELD_TYPE=>DB_DEF_BOOLEAN),
 	'select_email'=>array(
 		DB_FIELD_TYPE=>DB_DEF_BOOLEAN),
