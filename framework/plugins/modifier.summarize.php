dc17239c10993661181cd87e070a8342f667c822
dleffler <dleffler@hughes.net>
2013-01-11 15:12:46 -0500

    Adds new 'htmlpara' type to smarty 'summarize' modifier to leave html tags intact within the first p or div block and ensure all tags are closed appropriately.

--- framework/plugins/modifier.summarize.php
+++ framework/plugins/modifier.summarize.php
@@ -18,7 +18,8 @@
 
 /**
  * Smarty plugin
- * @package Smarty-Plugins
+ *
+ * @package    Smarty-Plugins
  * @subpackage Modifier
  */
 
@@ -36,36 +37,140 @@
  * @return array
  */
 function smarty_modifier_summarize($string, $strtype, $type) {
-	$sep = ($strtype == "html" ? array("</p>","</div>") : array("\r\n","\n","\r"));
-	$origstring = $string;
-	
-	switch ($type) {
-		case "para":
-			foreach ($sep as $s) {
-				$para = explode($s,$string);
-				$string = $para[0];
-			}
-			if (strlen($string) < strlen($origstring)-4) {$string .= " ...";}
+    $sep = ($strtype == "html" ? array("</p>", "</div>") : array("\r\n", "\n", "\r"));
+    $origstring = $string;
+
+    switch ($type) {
+        case "para":
+            foreach ($sep as $s) {
+                $para = explode($s, $string);
+                $string = $para[0];
+            }
+            if (strlen($string) < strlen($origstring)) {
+                $string .= " ...";
+            }
 //			return str_replace("&amp;#160;"," ",htmlentities(expString::convertSmartQuotes(strip_tags($string)),ENT_QUOTES));
-			return expString::convertSmartQuotes(strip_tags($string));
-			break;
-		case "paralinks":
-			foreach ($sep as $s) {
-				$para = explode($s,$string);
-				$string = $para[0];
-			}
-			if (strlen($string) < strlen($origstring)-4) {$string .= " ...";}
+            return expString::convertSmartQuotes(strip_tags($string));
+            break;
+        case "paralinks":
+            foreach ($sep as $s) {
+                $para = explode($s, $string);
+                $string = $para[0];
+            }
+            if (strlen($string) < strlen($origstring)) {
+                $string .= " ...";
+            }
 //			return str_replace("&#160;"," ",htmlspecialchars_decode(htmlentities(expString::convertSmartQuotes(strip_tags($string,'<a>')),ENT_QUOTES)));
-			return expString::convertSmartQuotes(strip_tags($string,'<a>'));
-			break;
-		default:
-			$words = explode(" ",strip_tags($string));
-			$string = implode(" ",array_slice($words,0,$type+0));
-			if (strlen($string) < strlen($origstring)-4) {$string .= " ...";}
+            return expString::convertSmartQuotes(strip_tags($string, '<a>'));
+            break;
+        case "parahtml":
+            foreach ($sep as $s) {
+                $para = explode($s, $string);
+                $string = $para[0];
+            }
+            if (strlen($string) < strlen($origstring)) {
+                $string .= " ...";
+            }
+            if (!empty($string)) {
+                $isText = true;
+                $ret = "";
+                $i = 0;
+//                    $currentChar = "";
+//                    $lastSpacePosition = -1;
+//                    $lastChar = "";
+                $tagsArray = array();
+                $currentTag = "";
+//                    $tagLevel = 0;
+//                    $noTagLength = strlen(strip_tags($string));
+
+                // Parser loop
+                for ($j = 0; $j < strlen($string); $j++) {
+
+                    $currentChar = substr($string, $j, 1);
+                    $ret .= $currentChar;
+
+                    // Lesser than event
+                    if ($currentChar == "<") $isText = false;
+
+                    // Character handler
+                    if ($isText) {
+
+                        // Memorize last space position
+                        if ($currentChar == " ") {
+                            $lastSpacePosition = $j;
+                        } else {
+                            $lastChar = $currentChar;
+                        }
+
+                        $i++;
+                    } else {
+                        $currentTag .= $currentChar;
+                    }
+
+                    // Greater than event
+                    if ($currentChar == ">") {
+                        $isText = true;
+
+                        // Opening tag handler
+                        if ((strpos($currentTag, "<") !== FALSE) &&
+                            (strpos($currentTag, "/>") === FALSE) &&
+                            (strpos($currentTag, "</") === FALSE)
+                        ) {
+
+                            // Tag has attribute(s)
+                            if (strpos($currentTag, " ") !== FALSE) {
+                                $currentTag = substr($currentTag, 1, strpos($currentTag, " ") - 1);
+                            } else {
+                                // Tag doesn't have attribute(s)
+                                $currentTag = substr($currentTag, 1, -1);
+                            }
+
+                            array_push($tagsArray, $currentTag);
+
+                        } else if (strpos($currentTag, "</") !== FALSE) {
+                            array_pop($tagsArray);
+                        }
+
+                        $currentTag = "";
+                    }
+                }
+                // Cut HTML string at last space position
+                //                if ($length < $noTagLength) {
+                //                    if ($lastSpacePosition != -1) {
+                //                        $ret = substr($string, 0, $lastSpacePosition);
+                //                    } else {
+                //                        $ret = substr($string, $j);
+                //                    }
+                //                }
+                if (sizeof($tagsArray) != 0) {
+                    // Close broken XHTML elements
+                    while (sizeof($tagsArray) != 0) {
+                        if (sizeof($tagsArray) > 1) {
+                            $aTag = array_pop($tagsArray);
+                            $string .= "</" . $aTag . ">";
+                        } // You may add more tags here to put the link and added text before the closing tag
+                        elseif ($aTag == 'p' || 'div') {
+                            $aTag = array_pop($tagsArray);
+                            $string .= "</" . $aTag . ">";
+                        } else {
+                            $aTag = array_pop($tagsArray);
+                            $string .= "</" . $aTag . ">";
+                        }
+                    }
+                }
+            }
+            return expString::convertSmartQuotes($string);
+            break;
+        default:
+            $words = explode(" ", strip_tags($string));
+            $string = implode(" ", array_slice($words, 0, $type + 0));
+            if (strlen($string) < strlen($origstring)) {
+                $string .= " ...";
+            }
 //			return str_replace("&amp;#160;"," ",htmlentities(expString::convertSmartQuotes($string),ENT_QUOTES));
-			return expString::convertSmartQuotes($string);
-			break;
-	}
+            return expString::convertSmartQuotes($string);
+            break;
+    }
 }
 
 ?>
