65eaa6ad76bba965870b3551ba00e530792bc3fd
dleffler <dleffler@hughes.net>
2013-01-16 07:17:34 -0500

    Fixes test problem left in text inline view

--- framework/modules/text/controllers/textController.php
+++ framework/modules/text/controllers/textController.php
@@ -51,7 +51,7 @@ class textController extends expController {
         if (expSession::is_set('uilevel')) {
         	$level = expSession::get('uilevel');
         }
-        $settings = $db->selectObject('htmleditor_ckeditor1', 'active=1');
+        $settings = $db->selectObject('htmleditor_ckeditor', 'active=1');
         if (empty($settings->data)) {
             $settings = new stdClass();
             $settings->data = "
