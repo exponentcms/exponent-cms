1a04e2e9b68ec2b37268393667d54b6331843b90
dleffler <dleffler@hughes.net>
2013-01-31 17:05:43 -0500

    Minor code tweaks to get merged develop code to integrate

--- framework/core/subsystems/expModules.php
+++ framework/core/subsystems/expModules.php
@@ -99,9 +99,9 @@ class expModules {
         
         foreach ($controllers as $module) {
     		if (class_exists($module)) {
-    			$mod = new $module();
+//    			$mod = new $module();
+                $mod = self::getController($module);
     			$modstate = $db->selectObject("modstate","module='$module'");
-
     			$moduleInfo[$module] = new stdClass();
     			$moduleInfo[$module]->class = $module;
     			$moduleInfo[$module]->name = $mod->name();
@@ -125,8 +125,8 @@ class expModules {
 
 	    $controllers = array();
 	    foreach($available_controllers as $name=>$path) {
-	        $controller = new $name();
-	        if (!empty($controller->useractions)) $controllers[] = $name;
+	        $controller = new $name();  // we want both models and controllers to filter out models
+	        if (!empty($controller->useractions)) $controllers[] = self::getControllerName($name);
 	    }
 
 	    return $controllers;
@@ -320,7 +320,8 @@ class expModules {
 	    $ctls = array();  // 2.0 modules
 	    foreach($modulestates as $state) {
 	        if (self::controllerExists($state->module)) {
-	            $controller = new $state->module();
+//	            $controller = new $state->module();
+                $controller = self::getController($state->module);
 	            if (!empty($controller->useractions)) {
 		            $ctls[] = $state->module;
 	            }
