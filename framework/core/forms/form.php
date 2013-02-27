24da5fd9aba188e6514c66b6ed4ad8d0d54fd08e
dleffler <dleffler@hughes.net>
2013-02-27 14:24:40 -0500

    Adds correct function to check for malformed control prior to adding it to a form to prevent the page crashing in rare instances when there is garbage in a control definition in the database

--- framework/core/forms/form.php
+++ framework/core/forms/form.php
@@ -74,7 +74,7 @@ class form extends baseform {
 		$this->controls[$name] = $control;
 		$this->controlLbl[$name] = $label;
         $this->tabs[$name] = $tab;
-		if (function_exists($control,'onRegister')) $control->onRegister($this);
+		if (method_exists($control,'onRegister')) $control->onRegister($this);
 		return true;
 	}
 
