0f50d9c6ac7090604d177769f6538842ccb60472
Phillip Ball <phillip@phillipball.com>
2013-03-04 13:21:45 -0800

    Fixes issue in Chrome: race condition where SimpleAjaxUploader doesn't load before the file manager code tries to call it. I've wrapped the code and modified it to become a YUI module, so that the code is loaded through the YUI dependency mechanisms, guaranteeing the code will be available when executed.

--- framework/core/subsystems/forms/controls/filemanagercontrol.php
+++ framework/core/subsystems/forms/controls/filemanagercontrol.php
@@ -71,7 +71,15 @@ class filemanagercontrol extends formcontrol {
         if (!empty($this->description)) $html .= "<br><div class=\"control-desc\">" . $this->description . "</div>";
         $html .= '</div>';
         $js = "
-            YUI(EXPONENT.YUI3_CONFIG).use('dd-constrain','dd-proxy','dd-drop','json','io', function(Y) {
+
+            EXPONENT.YUI3_CONFIG.modules.SimpleAjaxUploader = {
+                fullpath: EXPONENT.URL_FULL+'external/SimpleAjaxUploader.js'
+            };
+
+            YUI(EXPONENT.YUI3_CONFIG).use('dd-constrain','dd-proxy','dd-drop','json','io','SimpleAjaxUploader', function(Y) {
+
+                // console.log(ss);
+
                 var limit = ".$this->limit.";
                 var filesAdded = ".$this->count.";
                 var fl = Y.one('#filelist".$name."');
@@ -87,7 +95,7 @@ class filemanagercontrol extends formcontrol {
                 };
 
 //                  var quickUpload = new AjaxUpload($('#quickaddfiles-".$name."'), {
-                var quickUpload = new ss.SimpleUpload({
+                var quickUpload = new Y.ss.SimpleUpload({
                         button: '#quickaddfiles-".$name."',
                         action: '" . makelink(array("controller"=> "file", "action"=> "quickUpload", "ajax_action"=> 1, "json"=> 1)) . "',
                         data: {controller: 'file', action: 'quickUpload', ajax_action: 1, json: 1},
@@ -391,12 +399,12 @@ class filemanagercontrol extends formcontrol {
                 "yui3mods"=>"1",
                 "content"=>$js,
              ));
-            expJavascript::pushToFoot(array(
-                "unique"=>"quickupload",
-    //                "jquery"=>"1",
-    //                "src"=>PATH_RELATIVE."external/ajaxupload.3.5.js"
-                "src"=>PATH_RELATIVE."external/SimpleAjaxUploader.js"
-             ));
+    //         expJavascript::pushToFoot(array(
+    //             "unique"=>"quickupload",
+    // //                "jquery"=>"1",
+    // //                "src"=>PATH_RELATIVE."external/ajaxupload.3.5.js"
+    //             "src"=>PATH_RELATIVE."external/SimpleAjaxUploader.js"
+    //          ));
         return $html;
     }
     
