c0d3f6ee95a199d47ed9c6fbce1ff542a8300676
dleffler <dleffler@hughes.net>
2013-03-15 22:33:05 -0400

    Update version to 2.2.0beta1

--- exponent_version.php
+++ exponent_version.php
@@ -36,16 +36,16 @@ if (!defined('EXPONENT_VERSION_MAJOR')) {
 		/* exdoc
 		 * This specifies the type of release, either 'alpha','beta','release-candidate' or '' (for stable).
 		 */
-		define('EXPONENT_VERSION_TYPE','alpha');
+		define('EXPONENT_VERSION_TYPE','beta');
 		/* exdoc
 		 * This number is bumped each time a distribution of a single version is
 		 * released.  For instance, the 3rd beta has an version type iteration of 3.
 		 */
-		define('EXPONENT_VERSION_ITERATION','3'); // only applies to betas, alphas, or release candidates
+		define('EXPONENT_VERSION_ITERATION','1'); // only applies to betas, alphas, or release candidates
 		/* exdoc
 		 * This is the date that this version of Exponent was released.
 		 */
-		define('EXPONENT_VERSION_BUILDDATE','1362197380');
+		define('EXPONENT_VERSION_BUILDDATE','1363400928');
 	} else {
 		// the info for the "next" version if we are a pre-release from the repository
 		define('EXPONENT_VERSION_MAJOR','2');
