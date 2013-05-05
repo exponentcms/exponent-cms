1948477a734bb80cbfb55cc5b279e80e4ac92a6c
dleffler <dleffler@hughes.net>
2013-05-04 21:01:05 -0400

    Fixes db version check to complain with an error message if db version is less than 2.2.0release-candidate1 since any earlier version db will fail to display content until db is upgraded

--- framework/core/subsystems/expVersion.php
+++ framework/core/subsystems/expVersion.php
@@ -127,8 +127,18 @@ class expVersion {
 
         // check database version against installed software version
         if ($db->havedb) {
+            $dbversion = self::dbVersion();
+            $newversion = new stdClass();  // version of last major database change
+            $newversion->major = 2;
+            $newversion->minor = 2;
+            $newversion->revision = 0;
+            $newversion->type = 'release-candidate';
+            $newversion->iteration = '1';
+            $newversion->builddate = 0;
+            if (self::compareVersion($dbversion, $newversion)) {  // notice to upgrade to v2.2.0 if needed
+                flash('error', gt('The system database must be upgraded to display site content!'));
+            }
             if ($user->isSuperAdmin()) {
-                $dbversion = self::dbVersion();
                 // check if software version is newer than database version
                 if (self::compareVersion($dbversion, $swversion)) {
                     flash('message', gt('The database requires upgrading from') . ' v' . self::getDBVersion(true,false,true) . ' ' . gt('to') . ' v' . self::getVersion(true,false,true) .
