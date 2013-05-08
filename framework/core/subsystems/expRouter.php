c99e6e652aec527ef2bfc1d4b16badf103f08b74
dleffler <dleffler@hughes.net>
2013-05-08 15:03:52 -0400

    Fix a TON of warnings if there is a custom router_maps.php

--- framework/core/subsystems/expRouter.php
+++ framework/core/subsystems/expRouter.php
@@ -105,7 +105,7 @@ class expRouter {
                     for($i=0; $i < count($this->maps); $i++) {
                         $missing_params = array("dump");
 
-                        if(in_array($params['controller'], $this->maps[$i]) && in_array($params['action'], $this->maps[$i]) && (!isset($this->maps[$i]['src']) || in_array($params['src'], $this->maps[$i]))) {
+                        if (!empty($params) && (in_array($params['controller'], $this->maps[$i]) && in_array($params['action'], $this->maps[$i]) && (!isset($this->maps[$i]['src']) || in_array($params['src'], $this->maps[$i])))) {
                             $missing_params = array_diff_key($this->maps[$i]['url_parts'], $params);
                         }
 
