b2f08a2f1502b16556fb42ad7941a056f358f05a
dleffler <dleffler@hughes.net>
2013-05-14 08:02:07 -0400

    Upgrade to jqueryui v1.10.3

--- exponent_constants.php
+++ exponent_constants.php
@@ -484,12 +484,13 @@ if (!defined('JQUERY_RELATIVE')) {
 	 * Changing the version here lets Exponent adjust where to look
 	 */
 	define('JQUERY_VERSION', '1.9.1');
-    define('JQUERYUI_VERSION', '1.10.2');
+    define('JQUERYUI_VERSION', '1.10.3');
 	define('JQUERY_RELATIVE', PATH_RELATIVE.'external/jquery/');
     define('JQUERY_PATH', BASE.'external/jquery/');
 	define('JQUERY_URL', URL_FULL.'external/jquery/');
     define('JQUERY_SCRIPT', JQUERY_RELATIVE.'js/jquery-'.JQUERY_VERSION.'.min.js');
-    define('JQUERYUI_SCRIPT', JQUERY_RELATIVE.'js/jquery-ui-'.JQUERYUI_VERSION.'.custom.min.js');
+//    define('JQUERYUI_SCRIPT', JQUERY_RELATIVE.'js/jquery-ui-'.JQUERYUI_VERSION.'.custom.min.js');
+    define('JQUERYUI_SCRIPT', JQUERY_RELATIVE.'js/jquery-ui.min.js');
 }
 
 if (!defined('SMARTY_PATH')) {
@@ -507,7 +508,7 @@ if (!defined('SWIFT_PATH')) {
 	 * Swift Version Constant
 	 * Changing the version here lets Exponent adjust where to look
 	 */
-	define('SWIFT_VERSION', '4.3.1');
+	define('SWIFT_VERSION', '5.0.0');
 	define('SWIFT_PATH', BASE.'external/Swift-'.SWIFT_VERSION.'/lib/');
 }
 
