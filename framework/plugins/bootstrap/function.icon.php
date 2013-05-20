41402466acb8f279b80e06dee12d08316336fc6a
dleffler <dleffler@hughes.net>
2013-05-20 18:25:54 -0400

    Add standard 'confirm delete' if the icon class is set to delete or merge and not necessarily only the action

--- framework/plugins/bootstrap/function.icon.php
+++ framework/plugins/bootstrap/function.icon.php
@@ -203,7 +203,7 @@ if (!function_exists('smarty_function_icon')) {
                 $params['action'] = 'edit';
             }
             echo '<a href="' . expCore::makeLink($params) . '" title="' . $title . '" class=" btn '.$btn_type.' '.$btn_size.'"';
-            if (($params['action'] == "delete" || $params['action'] == "merge") && empty($onclick))
+            if (($params['action'] == "delete" || $params['action'] == "merge" || $class == "delete" || $class == "merge") && empty($onclick))
                 echo ' onclick="return confirm(\'' . gt('Are you sure you want to') . ' ' . $params['action'] . ' ' . gt('this') . ' ' . $smarty->getTemplateVars('modelname') . ' ' . gt('item') . '?\');"';
 //            if ($params['action'] == "merge" && empty($onclick))
 //                echo ' onclick="return confirm(\'' . gt('Are you sure you want to merge this') . ' ' . $smarty->getTemplateVars('modelname') . ' ' . gt('item') . '?\');"';
