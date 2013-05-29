1c3677d2dad979984333a358f861ac43b06e0c5f
dleffler <dleffler@hughes.net>
2013-05-29 15:37:27 -0400

    Prep table for nested help [#973]

--- framework/modules/help/definitions/help.php
+++ framework/modules/help/definitions/help.php
@@ -76,7 +76,10 @@ return array(
 	'location_data'=>array(
 		DB_FIELD_TYPE=>DB_DEF_STRING,
 		DB_FIELD_LEN=>250,
-		DB_INDEX=>10)
+		DB_INDEX=>10),
+    'parent'=>array(
+   		DB_FIELD_TYPE=>DB_DEF_ID,
+   		DB_INDEX=>10),
 );
 
 ?>
