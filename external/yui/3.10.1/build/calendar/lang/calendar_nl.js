c78ffce7b669eb196f741266e3219615b28b3b91
dleffler <dleffler@hughes.net>
2013-06-08 08:58:11 -0400

    Removes YUI v3.10.1

--- external/yui/3.10.1/build/calendar/lang/calendar_nl.js
+++
@@ -1,8 +0,0 @@
-/*
-YUI 3.10.1 (build 8bc088e)
-Copyright 2013 Yahoo! Inc. All rights reserved.
-Licensed under the BSD License.
-http://yuilibrary.com/license/
-*/
-
-YUI.add("lang/calendar_nl",function(e){e.Intl.add("calendar","nl",{weekdays:["zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag"],short_weekdays:["zon","maan","dins","woens","don","vrij","zat"],very_short_weekdays:["zo","ma","di","woe","do","vr","za"]})},"3.10.1");
