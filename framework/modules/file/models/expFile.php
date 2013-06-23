f1cb9b1ea0922866498f0e7029180193a7eaa46b
dleffler <dleffler@hughes.net>
2013-06-23 17:28:40 -0400

    Move getMimeType method into expFile since it's pretty generic and we've already altered it (cherry picked from commit 4126259)

--- framework/modules/file/models/expFile.php
+++ framework/modules/file/models/expFile.php
@@ -739,9 +739,84 @@ class expFile extends expRecord {
      * @return string
      */
     public static function getMimeType($filename) {
-        include_once(BASE.'external/class.mimeType.php');
-        $mime = new mimeType();
-        return $mime->getMimeType($filename);
+        /* Store an array of commom mimetypes */
+        $types = array(
+        'txt' => 'text/plain',
+        'htm' => 'text/html',
+        'html' => 'text/html',
+        'php' => 'text/html',
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'json' => 'application/json',
+        'xml' => 'application/xml',
+
+        // images
+        'png' => 'image/png',
+        'jpe' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'jpg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'bmp' => 'image/bmp',
+        'ico' => 'image/vnd.microsoft.icon',
+        'tiff' => 'image/tiff',
+        'tif' => 'image/tiff',
+        'svg' => 'image/svg+xml',
+        'svgz' => 'image/svg+xml',
+
+        // archives
+        'zip' => 'application/zip',
+        'rar' => 'application/x-rar-compressed',
+        'exe' => 'application/x-msdownload',
+        'msi' => 'application/x-msdownload',
+        'cab' => 'application/vnd.ms-cab-compressed',
+
+        // audio/video
+        'mp3' => 'audio/mpeg',
+        'qt' => 'video/quicktime',
+        'mov' => 'video/quicktime',
+        'ogg'  => 'audio/ogg',
+        'f4v'  => 'video/mp4',
+        'mp4'  => 'video/mp4',
+        'ogv'  => 'video/ogg',
+        '3gp'  => 'video/3gpp',
+        'webm' => 'video/webm',
+        'swf' => 'application/x-shockwave-flash',
+        'flv' => 'video/x-flv',
+
+        // adobe
+        'pdf' => 'application/pdf',
+        'psd' => 'image/vnd.adobe.photoshop',
+        'ai' => 'application/postscript',
+        'eps' => 'application/postscript',
+        'ps' => 'application/postscript',
+
+        // ms office
+        'doc' => 'application/msword',
+        'rtf' => 'application/rtf',
+        'xls' => 'application/vnd.ms-excel',
+        'ppt' => 'application/vnd.ms-powerpoint',
+
+        // open office
+        'odt' => 'application/vnd.oasis.opendocument.text',
+        'ods' => 'application/vnd.oasis.opendocument.spreadsheet');
+
+        /* Get the file extension,
+         * FYI: this is *really* hax.
+         */
+        $extension = strtolower(array_pop(explode('.',$filename)));
+        if(function_exists('finfo_open')) {
+            /* If we don't have to guess, do it the right way */
+            $finfo = finfo_open(FILEINFO_MIME);
+            $mimetype = finfo_file($finfo, $filename);
+            finfo_close($finfo);
+            return $mimetype;
+        } elseif(array_key_exists($extension, $types)) {
+            /* If we can *guess* the mimetype based on the filename, do that */
+            return $types[$extension];
+        } else {
+            /* Otherwise, let the browser guess */
+            return 'application/octet-stream';
+        }
     }
 
 // ==========================================================
