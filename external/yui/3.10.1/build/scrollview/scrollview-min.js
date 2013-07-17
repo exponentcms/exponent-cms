80df7d80473bc540403fe0717b0e1142ff5a473a
dleffler <dleffler@hughes.net>
2013-07-17 12:08:31 -0400

    Removes YUI v3.10.1

--- external/yui/3.10.1/build/scrollview/scrollview-min.js
+++
@@ -1,8 +0,0 @@
-/*
-YUI 3.10.1 (build 8bc088e)
-Copyright 2013 Yahoo! Inc. All rights reserved.
-Licensed under the BSD License.
-http://yuilibrary.com/license/
-*/
-
-YUI.add("scrollview",function(e,t){e.Base.plug(e.ScrollView,e.Plugin.ScrollViewScrollbars)},"3.10.1",{requires:["scrollview-base","scrollview-scrollbars"]});
