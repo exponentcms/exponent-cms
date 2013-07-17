f35276650f1f61dc2e911f247dfffaa3c2c5a2a9
dleffler <dleffler@hughes.net>
2013-07-17 10:59:16 -0400

    Updates to YUI v3.11.0

---
+++ external/yui/3.11.0/build/autocomplete-list/lang/autocomplete-list_es.js
@@ -0,0 +1,8 @@
+/*
+YUI 3.11.0 (build d549e5c)
+Copyright 2013 Yahoo! Inc. All rights reserved.
+Licensed under the BSD License.
+http://yuilibrary.com/license/
+*/
+
+YUI.add("lang/autocomplete-list_es",function(e){e.Intl.add("autocomplete-list","es",{item_selected:"{item} seleccionado.",items_available:"Hay sugerencias disponibles.  Use flecha arriba y abajo para seleccionar."})},"3.11.0");
---
+++ external/yui/3.11.0/build/selector/selector.js
@@ -0,0 +1,12 @@
+/*
+YUI 3.11.0 (build d549e5c)
+Copyright 2013 Yahoo! Inc. All rights reserved.
+Licensed under the BSD License.
+http://yuilibrary.com/license/
+*/
+
+YUI.add('selector', function (Y, NAME) {
+
+
+
+}, '3.11.0', {"requires": ["selector-native"]});
