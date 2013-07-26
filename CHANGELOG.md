15cc63d56873ddb32f00eb4b4b79b51b261ad3af
dleffler <dleffler@hughes.net>
2013-07-25 22:35:07 -0400

    Initial bumps for v2.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,17 @@ Copyright (c) 2004-2013 OIC Group, Inc.
 
 For a more detailed changelog visit https://github.com/exponentcms/exponent-cms/commits/master
 
+Added / Fixed / changed for 2.2.2
+-----------------------
+### Address any issues in v2.2.1
+  - includes all fixes from v2.2.1 patches
+### Known Issues
+  - 0.9x theme support is deprecated but still present in this distribution, it will be removed in the near future
+  - Flowplayer & YouTube modules are deprecated but still present in this distribution, they will be removed in the near future
+    -- There is no media player migration script, but you can migrate to flowplayer/youtube modules, then run the media player upgrade script
+  - eCommerce gift card & purchase order functionality have not been tested/may not be complete
+  - Item re-ranking (ddrerank) doesn't account for aggregation
+  - LDAP support may not work in all LDAP server scenarios due to limited testing
 
 Added / Fixed / changed for 2.2.1
 -----------------------
--- exponent_version.php
+++ exponent_version.php
@@ -30,7 +30,68 @@ if (!defined('EXPONENT_VERSION_MAJOR')) {
 		 */
 		define('EXPONENT_VERSION_MINOR','2');
 		/* exdoc
-		 * This is the revision version number of Exponent; the 2 in 1.96.2-beta3
+		 * This is the revision version number of Exponent; the 2 in 1.96.2-beta3<?php
+
+		 ##################################################
+		 #
+		 # Copyright (c) 2004-2013 OIC Group, Inc.
+		 #
+		 # This file is part of Exponent
+		 #
+		 # Exponent is free software; you can redistribute
+		 # it and/or modify it under the terms of the GNU
+		 # General Public License as published by the Free
+		 # Software Foundation; either version 2 of the
+		 # License, or (at your option) any later version.
+		 #
+		 # GPL: http://www.gnu.org/licenses/gpl.txt
+		 #
+		 ##################################################
+
+		 if (!defined('EXPONENT_VERSION_MAJOR')) {
+		 	// the RELEASE constant is changed by the build scripts at code freeze
+		 	define('RELEASE','%%RELEASE%%');
+		 	if (RELEASE != '%%RELEASE%%') {
+
+		 		/* exdoc
+		 		 * This is the major version number of Exponent; the 1 in 1.96.2-beta3
+		 		 */
+		 		define('EXPONENT_VERSION_MAJOR','%%MAJOR%%');
+		 		/* exdoc
+		 		 * This is the minor version number of Exponent; the 96 in 1.96.2-beta3
+		 		 */
+		 		define('EXPONENT_VERSION_MINOR','%%MINOR%%');
+		 		/* exdoc
+		 		 * This is the revision version number of Exponent; the 2 in 1.96.2-beta3
+		 		 */
+		 		define('EXPONENT_VERSION_REVISION','%%REVISION%%');
+		 		/* exdoc
+		 		 * This specifies the type of release, either 'alpha','beta','release-candidate' or '' (for stable).
+		 		 */
+		 		define('EXPONENT_VERSION_TYPE','%%TYPE%%');
+		 		/* exdoc
+		 		 * This number is bumped each time a distribution of a single version is
+		 		 * released.  For instance, the 3rd beta has an version type iteration of 3.
+		 		 */
+		 		define('EXPONENT_VERSION_ITERATION','%%ITERATION%%'); // only applies to betas, alphas, or release candidates
+		 		/* exdoc
+		 		 * This is the date that this version of Exponent was released.
+		 		 */
+		 		define('EXPONENT_VERSION_BUILDDATE','%%BUILDDATE%%');
+		 	} else {
+		 		// the info for the "next" version if we are a pre-release from the repository
+		 		define('EXPONENT_VERSION_MAJOR','2');
+		 		define('EXPONENT_VERSION_MINOR','2');
+		 		define('EXPONENT_VERSION_REVISION','2');
+		 		define('EXPONENT_VERSION_TYPE','develop');
+		 		define('EXPONENT_VERSION_ITERATION','');
+		 		define('EXPONENT_VERSION_BUILDDATE',time());
+		         // set DEVELOPMENT to 1 to debug the install process
+		 //        define('DEVELOPMENT','1');
+		 	}
+		 }
+
+		 ?>
 		 */
 		define('EXPONENT_VERSION_REVISION','1');
 		/* exdoc
