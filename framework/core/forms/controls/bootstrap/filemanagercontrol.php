a244b32348d52be75c9326c9e938f18b6f1a071f
dleffler <dleffler@hughes.net>
2013-07-29 19:14:29 -0400

    Adds error-reporting to quick-add uploader [#1062 state:resolved]

--- framework/core/forms/controls/bootstrap/filemanagercontrol.php
+++ framework/core/forms/controls/bootstrap/filemanagercontrol.php
@@ -162,6 +162,12 @@ class filemanagercontrol extends formcontrol {
                             Y.one('#quickaddfiles-".$name."').removeClass('quick-upload-disabled');
                         }
                     },
+                    onSizeError: function(filename, fileSize){
+                        alert(filename+' ".gt('is is too large to upload')."');
+                    },
+                    onError: function(filename, errorType, response){
+                        alert(filename+' '+errorType+' '+response);
+                    },
                 });
 
                 var listenForAdder = function(){
