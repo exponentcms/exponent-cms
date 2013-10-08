1cef9bc1b96e58792ed20d40778fee6a8cf21ba7
dleffler <dleffler@hughes.net>
2013-10-08 15:34:04 -0400

    Removes yui v3.11.0

--- external/yui/3.11.0/build/datatable-sort/lang/datatable-sort.js
+++
@@ -1,8 +0,0 @@
-/*
-YUI 3.11.0 (build d549e5c)
-Copyright 2013 Yahoo! Inc. All rights reserved.
-Licensed under the BSD License.
-http://yuilibrary.com/license/
-*/
-
-YUI.add("lang/datatable-sort",function(e){e.Intl.add("datatable-sort","",{asc:"Ascending",desc:"Descending",sortBy:"Sort by {column}",reverseSortBy:"Reverse sort by {column}"})},"3.11.0");
