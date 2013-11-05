d42a98abd2295c9fda5d4125022e30d979018249
dleffler <dleffler@hughes.net>
2013-11-05 10:29:02 -0500

    prevents adding an emtpy search query to the search query history list (cherry picked from commit dcfdb8f)

--- framework/modules/search/models/search.php
+++ framework/modules/search/models/search.php
@@ -38,7 +38,7 @@ class search extends expRecord {
         // get the search terms
         //$terms = $this->params['search_string'];
 
-        if (SAVE_SEARCH_QUERIES && $readonly == 0) {
+        if (SAVE_SEARCH_QUERIES && $readonly == 0 && !empty($terms)) {
             if (INCLUDE_ANONYMOUS_SEARCH == 1 || $user->id <> 0) {
                 $queryObj = new stdClass();
                 $queryObj->user_id = $user->id;
