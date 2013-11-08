ad51aec1c16fe35f111e6caa47e0e8bc0b404362
dleffler <dleffler@hughes.net>
2013-11-08 15:48:01 -0500

    Update smarty linkify modifier to be php v5.2 compliant (cherry picked from commit 1045911)

--- framework/plugins/modifier.linkify.php
+++ framework/plugins/modifier.linkify.php
@@ -17,22 +17,25 @@
 
 /**
  * Smarty plugin
- * @package Smarty-Plugins
+ *
+ * @package    Smarty-Plugins
  * @subpackage Modifier
  */
 
 /**
  * Smarty regex_replace modifier plugin
- *
  * Type:     modifier<br>
  * Name:     regex_replace<br>
  * Purpose:  regular expression search/replace
- * @link http://smarty.php.net/manual/en/language.modifier.regex.replace.php
- *          regex_replace (Smarty online manual)
+ *
+ * @link     http://smarty.php.net/manual/en/language.modifier.regex.replace.php
+ *           regex_replace (Smarty online manual)
  * @author   Monte Ohrt <monte at ohrt dot com>
+ *
  * @param string
- * @param string|array
- * @param string|array
+ * @param    string|array
+ * @param    string|array
+ *
  * @return string
  */
 /**
@@ -45,9 +48,13 @@
  * @internal param string $mode normal or all
  * @return string
  */
-function smarty_modifier_linkify($value, $protocols = array('http', 'mail'),
-        array $attributes = array())
-{
+function smarty_modifier_linkify(
+    $value,
+    $protocols = array('http', 'mail'),
+    array $attributes = array()
+) {
+    global $attr, $links, $protocol;
+
     // Link attributes
     $attr = '';
     foreach ($attributes as $key => $val) {
@@ -57,73 +64,120 @@ function smarty_modifier_linkify($value, $protocols = array('http', 'mail'),
     $links = array();
 
     // Extract existing links and tags
-    $value = preg_replace_callback('~(<a .*?>.*?</a>|<.*?>)~i',
-            function ($match) use (&$links)
-            {
-                return '<' . array_push($links, $match[1]) . '>';
-            }, $value);
+    $value = preg_replace_callback(
+        '~(<a .*?>.*?</a>|<.*?>)~i',
+        "existing_links",
+        $value
+    );
 
     // Extract text links for each protocol
-    foreach ((array) $protocols as $protocol) {
+    foreach ((array)$protocols as $protocol) {
+        global $protocol1;
+
+        $protocol1 = $protocol;
+
         switch ($protocol) {
-        case 'http':
-        case 'https':
-            $value = preg_replace_callback(
+            case 'http':
+            case 'https':
+                $value = preg_replace_callback(
                     '~(?:(https?)://([^\s<]+)|(www\.[^\s<]+?\.[^\s<]+))(?<![\.,:])~i',
-                    function ($match) use ($protocol, &$links, $attr)
-                    {
-                        if ($match[1])
-                            $protocol = $match[1];
-                        $link = $match[2] ? : $match[3];
-                        return '<'
-                                . array_push($links,
-                                        "<a $attr href=\"$protocol://$link\">$link</a>")
-                                . '>';
-                    }, $value);
-            break;
-        case 'mail':
-            $value = preg_replace_callback(
+                    "http_link",
+                    $value
+                );
+                break;
+            case 'mail':
+                $value = preg_replace_callback(
                     '~([^\s<]+?@[^\s<]+?\.[^\s<]+)(?<![\.,:])~',
-                    function ($match) use (&$links, $attr)
-                    {
-                        return '<'
-                                . array_push($links,
-                                        "<a $attr href=\"mailto:{$match[1]}\">{$match[1]}</a>")
-                                . '>';
-                    }, $value);
-            break;
-        case 'twitter':
-            $value = preg_replace_callback('~(?<!\w)[@#](\w++)~',
-                    function ($match) use (&$links, $attr)
-                    {
-                        return '<'
-                                . array_push($links,
-                                        "<a $attr href=\"https://twitter.com/"
-                                                . ($match[0][0] == '@' ? ''
-                                                        : 'search/%23')
-                                                . $match[1]
-                                                . "\">{$match[0]}</a>") . '>';
-                    }, $value);
-            break;
-        default:
-            $value = preg_replace_callback(
+                    "mail_link",
+                    $value
+                );
+                break;
+            case 'twitter':
+                $value = preg_replace_callback(
+                    '~(?<!\w)[@#](\w++)~',
+                    "twitter_link",
+                    $value
+                );
+                break;
+            default:
+                $value = preg_replace_callback(
                     '~' . preg_quote($protocol, '~')
-                            . '://([^\s<]+?)(?<![\.,:])~i',
-                    function ($match) use ($protocol, &$links, $attr)
-                    {
-                        return '<'
-                                . array_push($links,
-                                        "<a $attr href=\"$protocol://{$match[1]}\">{$match[1]}</a>")
-                                . '>';
-                    }, $value);
-            break;
+                    . '://([^\s<]+?)(?<![\.,:])~i',
+                    "other_link",
+                    $value
+                );
+                break;
         }
     }
 
     // Insert all link
-    return preg_replace_callback('/<(\d+)>/',
-            function ($match) use (&$links)
-            {
-                return $links[$match[1] - 1];
-            }, $value);
+    return preg_replace_callback(
+        '/<(\d+)>/',
+        "insert_link",
+        $value
+    );
 }
+
+function existing_links($match) {
+    global $links;
+
+    return '<' . array_push($links, $match[1]) . '>';
+}
+
+function http_link($match) {
+    global $protocol1, $links, $attr;
+
+    if ($match[1]) {
+        $protocol1 = $match[1];
+    }
+    $link = $match[2] ? $match[2] : $match[3];
+    return '<'
+    . array_push(
+        $links,
+        "<a $attr href=\"$protocol1://$link\">$link</a>"
+    )
+    . '>';
+}
+
+function mail_link($match) {
+    global $links, $attr;
+
+    return '<'
+    . array_push(
+        $links,
+        "<a $attr href=\"mailto:{$match[1]}\">{$match[1]}</a>"
+    )
+    . '>';
+}
+
+function twitter_link($match) {
+    global $links, $attr;
+
+    return '<'
+    . array_push(
+        $links,
+        "<a $attr href=\"https://twitter.com/"
+        . ($match[0][0] == '@' ? ''
+            : 'search/%23')
+        . $match[1]
+        . "\">{$match[0]}</a>"
+    ) . '>';
+}
+
+function other_link($match) {
+    global $protocol1, $links, $attr;
+
+    return '<'
+    . array_push(
+        $links,
+        "<a $attr href=\"$protocol1://{$match[1]}\">{$match[1]}</a>"
+    )
+    . '>';
+}
+
+function insert_link($match) {
+    global $links;
+
+    return $links[$match[1] - 1];
+}
+
