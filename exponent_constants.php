6883f13bac57bc16935e3e0796d63883b6f86e66
dleffler <dleffler@hughes.net>
2013-12-06 12:06:17 -0500

    Update to SwiftMailer v5.0.3

--- exponent_constants.php
+++ exponent_constants.php
@@ -532,7 +532,7 @@ if (!defined('SWIFT_PATH')) {
 	 * Swift Version Constant
 	 * Changing the version here lets Exponent adjust where to look
 	 */
-	define('SWIFT_VERSION', '5.0.2');
+	define('SWIFT_VERSION', '5.0.3');
 	define('SWIFT_PATH', BASE.'external/Swift-'.SWIFT_VERSION.'/lib/');
 }
 
