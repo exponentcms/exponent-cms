22ada7bbc50f98f0042902ae69a5c34c234a5226
dleffler <dleffler@hughes.net>
2013-12-10 11:55:46 -0500

    Updates bootstrap theme to be more 'mobile' friendly now main content moves to top on narrow displays (instead of sidebar) [#1114]

--- themes/bootstraptheme/subthemes/Follow Parent.php
+++ themes/bootstraptheme/subthemes/Follow Parent.php
@@ -40,12 +40,12 @@
             <!-- optional flyout sidebar container -->
             <?php expTheme::module(array("controller"=>"navigation","action"=>"showall","view"=>"showall_flyout_sidebar","source"=>"navsidebar","chrome"=>true)); ?>
             <section id="main" class="row">
-                <aside id="sidebar" class="span3 well">
-                    <?php expTheme::module(array("controller"=>"container","action"=>"showall","view"=>"showall","source"=>"@left","scope"=>"top-sectional")); ?>
-                </aside>
-                <section id="content" class="span8">
+                <section id="content" class="span8 pull-right">
                     <?php expTheme::main(); ?>
                 </section>
+                <aside id="sidebar" class="span3 well pull-left">
+                    <?php expTheme::module(array("controller"=>"container","action"=>"showall","view"=>"showall","source"=>"@left","scope"=>"top-sectional")); ?>
+                </aside>
             </section>
             <footer class="row">
                 <?php expTheme::module(array("controller"=>"text","action"=>"showall","view"=>"showall_single","source"=>"@footer","chrome"=>1)) ?>
