942b9d93bfa6bd0b425b4cc68506ff3386ff7e63
dleffler <dleffler@hughes.net>
2014-03-11 11:48:07 -0400

    Organizes system settings defaults files (/conf/extensions) so they more closely conform to the site configuration tabs

--- framework/conf/extensions/display.defaults.php
+++ framework/conf/extensions/display.defaults.php
@@ -17,17 +17,19 @@
 ##################################################
 
 if (!defined('LANGUAGE')) define('LANGUAGE','English - US');
-if (!defined('SLINGBAR_TOP')) define('SLINGBAR_TOP','1');
 if (!defined('DISPLAY_THEME_REAL')) define('DISPLAY_THEME_REAL','retrotheme');
-if (!defined('FORCE_MOBILE')) define('FORCE_MOBILE','0');
 if (!defined('THEME_STYLE_REAL')) define('THEME_STYLE_REAL','');
+if (!defined('INVERT_HIDE_TITLE')) define('INVERT_HIDE_TITLE','0');
+if (!defined('FORCE_MOBILE')) define('FORCE_MOBILE','0');
+
 if (!defined('DISPLAY_ATTRIBUTION')) define('DISPLAY_ATTRIBUTION','username');
 if (!defined('DISPLAY_DATETIME_FORMAT')) define('DISPLAY_DATETIME_FORMAT','%m/%d/%y %I:%M%p');
 if (!defined('DISPLAY_DATE_FORMAT')) define('DISPLAY_DATE_FORMAT','%m/%d/%y');
 if (!defined('DISPLAY_TIME_FORMAT')) define('DISPLAY_TIME_FORMAT','%I:%M%p');
 if (!defined('DISPLAY_START_OF_WEEK')) define('DISPLAY_START_OF_WEEK','0');
+
 if (!defined('DISPLAY_DEFAULT_TIMEZONE')) define('DISPLAY_DEFAULT_TIMEZONE','UTC');
+if (!defined('SLINGBAR_TOP')) define('SLINGBAR_TOP','1');
 if (!defined('THUMB_QUALITY')) define('THUMB_QUALITY','75');
-if (!defined('INVERT_HIDE_TITLE')) define('INVERT_HIDE_TITLE','0');
 
 ?>
---
+++ framework/conf/extensions/error.defaults.php
@@ -0,0 +1,24 @@
+<?php
+
+##################################################
+#
+# Copyright (c) 2004-2014 OIC Group, Inc.
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
+if (!defined('SITE_404_TITLE')) define('SITE_404_TITLE','Page Not Found');
+if (!defined('SITE_404_HTML')) define('SITE_404_HTML',exponent_unhtmlentities('The page you were looking for wasn&apos;t found.  It may have been moved or deleted.'));
+if (!defined('SITE_403_REAL_HTML')) define('SITE_403_REAL_HTML',exponent_unhtmlentities('<h3>Authorization Failed</h3>You are not allowed to perform this operation.'));
+if (!defined('SESSION_TIMEOUT_HTML')) define('SESSION_TIMEOUT_HTML',exponent_unhtmlentities('<h3>Expired Login Session</h3>Your session has expired, because you were idle too long.  You will have to log back into the system to continue what you were doing.'));
+
+?>
