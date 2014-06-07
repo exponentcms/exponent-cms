13bfbba2971c676abd0857d389876480818b24a5
dleffler <dleffler@hughes.net>
2014-06-07 18:28:30 -0400

    Updates country & region controls to fully mimic {control} plugin; updates countryregion control to work better; updates expGeo subsystem to abide by 'active' property

--- framework/core/subsystems/expGeo.php
+++ framework/core/subsystems/expGeo.php
@@ -18,81 +18,116 @@
 /**
  * This is the class expGeo
  *
- * @package Subsystems
+ * @package    Subsystems
  * @subpackage Subsystems
  */
+
 /** @define "BASE" "../../.." */
+class expGeo
+{
 
-class expGeo {
-          
-	/** exdoc
-	 * List all Countries in the Geo Database.  Returns an array of country objects.
-	 *
-	 * @node Subsystems:expGeo
-	 * @return array
-	 */
-	public static function listCountriesOnly() {
-		global $db;
-		$countries = array();
-		foreach ($db->selectObjects("geo_country") as $c) {
-			$countries[$c->id] = $c->name;
-		}
-		uasort($countries,"strnatcasecmp");
-		return $countries;
-	}
+    /** exdoc
+     * List active Countries in the Geo Database.  Returns an array of country objects.
+     *
+     * @node Subsystems:expGeo
+     * @return array
+     */
+    public static function listCountriesOnly()
+    {
+        global $db;
 
-	/** exdoc
-	 * List Regions for a specific Country. Returns an array of regions.
-	 *
-	 * @param integer $country_id The id of the country to get regions for
-	 * @node Subsystems:expGeo
-	 * @return array
-	 */
-	public static function listRegions($country_id) {
-		global $db;
-		$regions = array();
-		foreach ($db->selectObjects("geo_region","country_id=".$country_id) as $r) {
-			$regions[$r->id] = $r->name;
-		}
-		uasort($regions,"strnatcasecmp");
-		return $regions;
-	}
+        $countries = array();
+        foreach ($db->selectObjects("geo_country", "active=1") as $c) {
+            $countries[$c->id] = $c->name;
+        }
+        uasort($countries, "strnatcasecmp");
+        return $countries;
+    }
 
-	/** exdoc
-	 * List Countries and Regions in the Geo Database.  Returns a two-tiered array of countries and regions.
-	 * @node Subsystems:expGeo
-	 * @return array
-	 */
-	public static function listCountriesAndRegions() {
-		global $db;
-		$countries = array();
-		foreach ($db->selectObjects("geo_country") as $c) {
-			$countries[$c->id] = new stdClass();
-			$countries[$c->id]->name = $c->name;
-			$countries[$c->id]->regions = array();
+    /** exdoc
+     * List all Countries in the Geo Database.  Returns an array of country objects.
+     *
+     * @node Subsystems:expGeo
+     * @return array
+     */
+    public static function listAllCountriesOnly()
+    {
+        global $db;
 
-			foreach ($db->selectObjects("geo_region","country_id=".$c->id) as $r) {
-				$countries[$c->id]->regions[$r->id] = $r->name;
-			}
-			uasort($countries[$c->id]->regions,"strnatcasecmp");
-		}
-		$countries = expSorter::sort(array('array'=>$countries,'sortby'=>'name', 'order'=>'ASC', 'ignore_case'=>true, 'type'=>'a'));
-		return $countries;
-	}
+        $countries = array();
+        foreach ($db->selectObjects("geo_country") as $c) {
+            $countries[$c->id] = $c->name;
+        }
+        uasort($countries, "strnatcasecmp");
+        return $countries;
+    }
 
-	/** exdoc
-	 * List all Regions in the Geo Database.  Returns an array of regions
-	 * @node Subsystems:expGeo
-	 * @return array
-	 */
-	public static function listAllRegions() {
-		global $db;
-		$regions = array();
-		foreach ($db->selectObjects("geo_region") as $r) {
-			$regions[$r->id] = $r->name;
-		}
-		return $regions;
-	}
+    /** exdoc
+     * List active Regions for a specific Country. Returns an array of region objects.
+     *
+     * @param integer $country_id The id of the country to get regions for
+     *
+     * @node Subsystems:expGeo
+     * @return array
+     */
+    public static function listRegions($country_id, $include_blank = null)
+    {
+        global $db;
+
+        $regions = array();
+        foreach ($db->selectObjects("geo_region", "country_id=" . $country_id . " AND active=1") as $r) {
+            $regions[$r->id] = $r->name;
+        }
+        uasort($regions, "strnatcasecmp");
+        if (!empty($regions) && !empty($include_blank)) {
+            array_unshift($regions, $include_blank);
+        }
+        return $regions;
+    }
+
+    /** exdoc
+     * List all Regions in the Geo Database.  Returns an array of regions
+     *
+     * @node Subsystems:expGeo
+     * @return array
+     */
+    public static function listAllRegions()
+    {
+        global $db;
+
+        $regions = array();
+        foreach ($db->selectObjects("geo_region") as $r) {
+            $regions[$r->id] = $r->name;
+        }
+        return $regions;
+    }
+
+    /** exdoc
+     * List active Countries and Regions in the Geo Database.  Returns a two-tiered array of countries and regions.
+     *
+     * @node Subsystems:expGeo
+     * @return array
+     */
+    public static function listCountriesAndRegions()
+    {
+        global $db;
+
+        $countries = array();
+        foreach ($db->selectObjects("geo_country", "active=1") as $c) {
+            $countries[$c->id] = new stdClass();
+            $countries[$c->id]->name = $c->name;
+            $countries[$c->id]->regions = array();
+
+            foreach ($db->selectObjects("geo_region", "country_id=" . $c->id . " AND active=1") as $r) {
+                $countries[$c->id]->regions[$r->id] = $r->name;
+            }
+            uasort($countries[$c->id]->regions, "strnatcasecmp");
+        }
+        $countries = expSorter::sort(
+            array('array' => $countries, 'sortby' => 'name', 'order' => 'ASC', 'ignore_case' => true, 'type' => 'a')
+        );
+        return $countries;
+    }
 
 }
 
