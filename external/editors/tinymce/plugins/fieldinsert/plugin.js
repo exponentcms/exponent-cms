a43bc8292e31c52b558f96dec81c75571dc0c839
dleffler <dleffler@hughes.net>
2014-07-16 21:38:25 -0400

    Updates TinyMCE editor to v4.1.2, adds language files (but we don't use them yet)

--- external/editors/tinymce/plugins/fieldinsert/plugin.js
+++ external/editors/tinymce/plugins/fieldinsert/plugin.js
@@ -11,99 +11,35 @@
 /*global tinymce:true */
 
 tinymce.PluginManager.add('fieldinsert', function(editor) {
-	var daysShort = "Sun Mon Tue Wed Thu Fri Sat Sun".split(' ');
-	var daysLong = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday Sunday".split(' ');
-	var monthsShort = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(' ');
-	var monthsLong = "January February March April May June July August September October November December".split(' ');
 	var menuItems = [], lastFormat;
-
-	function getDateTime(fmt, date) {
-		function addZeros(value, len) {
-			value = "" + value;
-
-			if (value.length < len) {
-				for (var i = 0; i < (len - value.length); i++) {
-					value = "0" + value;
-				}
-			}
-
-			return value;
-		}
-
-		date = date || new Date();
-
-		fmt = fmt.replace("%D", "%m/%d/%Y");
-		fmt = fmt.replace("%r", "%I:%M:%S %p");
-		fmt = fmt.replace("%Y", "" + date.getFullYear());
-		fmt = fmt.replace("%y", "" + date.getYear());
-		fmt = fmt.replace("%m", addZeros(date.getMonth() + 1, 2));
-		fmt = fmt.replace("%d", addZeros(date.getDate(), 2));
-		fmt = fmt.replace("%H", "" + addZeros(date.getHours(), 2));
-		fmt = fmt.replace("%M", "" + addZeros(date.getMinutes(), 2));
-		fmt = fmt.replace("%S", "" + addZeros(date.getSeconds(), 2));
-		fmt = fmt.replace("%I", "" + ((date.getHours() + 11) % 12 + 1));
-		fmt = fmt.replace("%p", "" + (date.getHours() < 12 ? "AM" : "PM"));
-		fmt = fmt.replace("%B", "" + editor.translate(monthsLong[date.getMonth()]));
-		fmt = fmt.replace("%b", "" + editor.translate(monthsShort[date.getMonth()]));
-		fmt = fmt.replace("%A", "" + editor.translate(daysLong[date.getDay()]));
-		fmt = fmt.replace("%a", "" + editor.translate(daysShort[date.getDay()]));
-		fmt = fmt.replace("%%", "%");
-
-		return fmt;
-	}
-
-	function insertDateTime(format) {
-		var html = getDateTime(format);
-
-		if (editor.settings.insertdatetime_element) {
-			var computerTime;
-
-			if (/%[HMSIp]/.test(format)) {
-				computerTime = getDateTime("%Y-%m-%dT%H:%M");
-			} else {
-				computerTime = getDateTime("%Y-%m-%d");
-			}
-
-			html = '<time datetime="' + computerTime + '">' + html + '</time>';
-
-			var timeElm = editor.dom.getParent(editor.selection.getStart(), 'time');
-			if (timeElm) {
-				editor.dom.setOuterHTML(timeElm, html);
-				return;
-			}
-		}
-
-		editor.insertContent(html);
-	}
 
     // Add a button that inserts time
 	editor.addButton('fieldinsert', {
-		type: 'splitbutton',
+		type: 'menubutton',
+//        text: 'Field',
+        icon: 'template',
 		title: 'Insert field',
-		onclick: function() {
-			insertDateTime(lastFormat || "%H:%M:%S");
-		},
+//		onclick: function() {
+//            insertField(lastFormat || "%H:%M:%S");
+//		},
 		menu: menuItems
 	});
 
-	tinymce.each(editor.settings.insertdatetime_formats || [
-		"%H:%M:%S",
-		"%Y-%m-%d",
-		"%I:%M:%S %p",
-		"%D"
-	], function(fmt) {
+	tinymce.each(editor.settings.fieldinsert_list || [
+        '', 'No Fields', 'There are no fields'
+	], function(field) {
 		menuItems.push({
-			text: getDateTime(fmt),
+			text: field[1],
 			onclick: function() {
-				lastFormat = fmt;
-				insertDateTime(fmt);
+//                lastFormat = field[0];
+                editor.insertContent(field[0]);
 			}
 		});
 	});
 
     // Adds a menu item to the insert menu
 	editor.addMenuItem('fieldinsert', {
-		icon: 'list',
+		icon: 'template',
 		text: 'Insert field',
 		menu: menuItems,
 		context: 'insert'
