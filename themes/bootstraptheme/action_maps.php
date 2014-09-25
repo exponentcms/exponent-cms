3a0981238b93f2fcf07e3388ba1320fd357bf46e
dleffler <dleffler@hughes.net>
2014-09-25 12:44:48 -0400

    Adds sample action_maps and router_maps to our sample themes...all mapping is commented out

---
+++ themes/bootstraptheme/action_maps.php
@@ -0,0 +1,43 @@
+<?php
+
+##################################################
+#
+# Copyright (c) 2004-2014 OIC Group, Inc.
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
+if (!defined('EXPONENT')) {
+    exit('');
+}
+
+// the module may be a wildcard (meaning all modules) by using an asterisk '*'
+// the action may be a wildcard (meaning all methods) by using an asterisk '*'
+return array(
+//    'cart'=>array(                      // the 'cartController' module
+//        'checkout'=>'Full Body'         // the 'checkout' method will use the 'Full Body' subtheme
+//    ),
+//    'order'=>array(                     // the 'orderController' module
+//        '*'=>'Full Body'                // ANY order method will use the 'Full Body' subtheme
+//    ),
+//    '*'=>array(                         // ANY module
+//        'showall_by_tags'=>'Tags View'  // ANY order method will use the 'Tags View' subtheme
+//    ),
+//    'news'=>array(
+//        'show'=>'Large Banner'
+//    ),
+//    'search'=>array(
+//        'search'=>'_Search Results'
+//    ),
+);
+
+?>
