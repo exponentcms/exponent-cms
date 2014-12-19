5e82db1b8cd6127b7661839e6662c7627d49bd0a
dleffler <dleffler@hughes.net>
2014-12-19 11:45:37 -0500

    Update 3rd party libraries; jquery to v1.11.2 & 2.1.3; swiftmailer to v5.3.1

--- exponent_constants.php
+++ exponent_constants.php
@@ -523,8 +523,8 @@ if (!defined('YUI2_RELATIVE')) {
  * Changing the version here lets Exponent adjust where to look
  */
 if (!defined('JQUERY_RELATIVE')) {
-    define('JQUERY_VERSION', '1.11.1');
-    define('JQUERY2_VERSION', '2.1.1');
+    define('JQUERY_VERSION', '1.11.2');
+    define('JQUERY2_VERSION', '2.1.3');
     define('JQUERYUI_VERSION', '1.11.2');
     define('JQUERY_RELATIVE', PATH_RELATIVE . 'external/jquery/');
     define('JQUERY_PATH', BASE . 'external/jquery/');
@@ -556,7 +556,7 @@ if (!defined('SMARTY_PATH')) {
  * Changing the version here lets Exponent adjust where to look
  */
 if (!defined('SWIFT_PATH')) {
-    define('SWIFT_VERSION', '5.3.0');
+    define('SWIFT_VERSION', '5.3.1');
     define('SWIFT_PATH', BASE . 'external/swiftmailer-' . SWIFT_VERSION . '/lib/');
 }
 
