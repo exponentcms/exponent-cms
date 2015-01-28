5749479b02fd46f0b1eeae170998cd97011f2b8a
dleffler <dleffler@hughes.net>
2015-01-28 10:01:17 -0500

    Updates ckeditor to v4.4.7

--- external/editors/ckeditor/plugins/widget/lang/sq.js
+++ external/editors/ckeditor/plugins/widget/lang/sq.js
@@ -2,6 +2,6 @@
  * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
-CKEDITOR.plugins.setLang( 'widget', 'ko', {
-	'move': '움직이려면 클릭 후 드래그 하세요'
+CKEDITOR.plugins.setLang( 'widget', 'sq', {
+	'move': 'Kliko dhe tërhiqe për ta lëvizur'
 } );
