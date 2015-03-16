5a412d03dc1a836309d7513d97d5a11c0bfc12df
dleffler <dleffler@hughes.net>
2015-03-15 20:59:46 -0400

    Update Swiftmailer to v5.4.0, fwiw, we blew php v5.2.x compatibility in Exponent v2.3.2 with the inclusion of this library v5.3.1

--- exponent_constants.php
+++ exponent_constants.php
@@ -556,7 +556,7 @@ if (!defined('SMARTY_PATH')) {
  * Changing the version here lets Exponent adjust where to look
  */
 if (!defined('SWIFT_PATH')) {
-    define('SWIFT_VERSION', '5.3.1');
+    define('SWIFT_VERSION', '5.4.0');
     define('SWIFT_PATH', BASE . 'external/swiftmailer-' . SWIFT_VERSION . '/lib/');
 }
 
