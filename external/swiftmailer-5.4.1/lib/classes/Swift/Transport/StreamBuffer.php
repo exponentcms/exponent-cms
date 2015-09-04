b810c92d0fa04af096be93a446660c8adfac59e9
dleffler <dleffler@hughes.net>
2015-09-04 15:39:13 -0400

    Code hack to allow expMail to work with ssl (and tls?) protocol when running under php v5.6.x

--- external/swiftmailer-5.4.1/lib/classes/Swift/Transport/StreamBuffer.php
+++ external/swiftmailer-5.4.1/lib/classes/Swift/Transport/StreamBuffer.php
@@ -257,6 +257,10 @@ class Swift_Transport_StreamBuffer extends Swift_ByteStream_AbstractFilterableIn
             $timeout = $this->_params['timeout'];
         }
         $options = array();
+        if (version_compare(PHP_VERSION, '5.6.0') >= 0) {
+            $options['ssl']['verify_peer'] = FALSE;
+            $options['ssl']['verify_peer_name'] = FALSE;
+        }
         if (!empty($this->_params['sourceIp'])) {
             $options['socket']['bindto'] = $this->_params['sourceIp'].':0';
         }
