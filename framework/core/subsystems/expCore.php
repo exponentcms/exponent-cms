d44d827ed44201e69d99af425b1f7b3ae9d2519a
dleffler <dleffler@hughes.net>
2015-12-24 10:27:03 -0500

    Adds expCore method to check for php version to help with code variances needed with different php versions

--- framework/core/subsystems/expCore.php
+++ framework/core/subsystems/expCore.php
@@ -431,6 +431,25 @@ class expCore
         return $destination;
     }
 
+    /**
+   	 * Determines if the current version of PHP is equal to or greater than the supplied value
+   	 *
+   	 * @param	string
+   	 * @return	bool	TRUE if the current version is $version or higher
+   	 */
+    public static function is_php($version)
+   	{
+   		static $_is_php;
+   		$version = (string) $version;
+
+   		if ( ! isset($_is_php[$version]))
+   		{
+   			$_is_php[$version] = version_compare(PHP_VERSION, $version, '>=');
+   		}
+
+   		return $_is_php[$version];
+   	}
+
 }
 
 ?>
