35a5aa64457adc84632a75dd6920121fa4df30a5
dleffler <dleffler@hughes.net>
2016-01-23 11:24:33 -0500

    Fix warnings in mp3file call

--- external/mp3file.php
+++ external/mp3file.php
@@ -419,19 +419,18 @@ class mp3file
     // functions
     function getTagsInfo() {
         // read source file
-//        $iFSize = filesize($sFilepath);
         $iFSize = $this->mp3data['Filesize'];
-//        $vFD = fopen($sFilepath,'r');
-//        $sSrc = fread($vFD,$iFSize);
-//        fclose($vFD);
         fseek($this->fd,0);
         $sSrc = fread($this->fd,$iFSize);
-
+        $aInfo = array();
 
         // obtain base info
         if (substr($sSrc,0,3) == 'ID3') {
 //            $aInfo['FileName'] = $sFilepath;
             $aInfo['ID3Version'] = hexdec(bin2hex(substr($sSrc,3,1))).'.'.hexdec(bin2hex(substr($sSrc,4,1)));
+            echo 'Success';
+        } else {
+            return $aInfo;
         }
 
         // passing through possible tags of idv2 (v3 and v4)
