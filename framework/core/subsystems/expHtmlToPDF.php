ac5c5c218f3f4aa8e4248fb3829e0276c5fe99cb
dleffler <dleffler@hughes.net>
2016-02-12 17:13:34 -0500

    Initial support for dompdf 0.7.0 in expHtmlToPDF subsystem...we will not expose this into site configuration until after it is released as stable...still some bugs & quirks which may require more configuration on our end

--- framework/core/subsystems/expHtmlToPDF.php
+++ framework/core/subsystems/expHtmlToPDF.php
@@ -720,6 +720,187 @@ class expDOMPDF extends expHtmlToPDF
 }
 
 /**
+ * This is the class expDOMPDF070
+ * a wrapper for using dompdf v0.7.0
+ *
+ * @package    Subsystems
+ * @subpackage Subsystems
+ */
+/** @define "BASE" "../../.." */
+
+class expDOMPDF070 extends expHtmlToPDF
+{
+
+    /**
+     * Return status of pdf engine being installed correctly
+     */
+    public static function installed() {
+        return file_exists(BASE . 'external/dompdf070/autoload.inc.php');
+    }
+
+    /**
+     * Constructor: initialize a pdf file file.
+     *
+     * @param string $paper_size  page size
+     * @param string $orientation page orientation
+     * @param string $html        html code for page
+     * @param bool   $use_file    a flag to show $html is an html file location to be loaded
+     */
+    public function __construct($paper_size = "A4", $orientation = "portrait", $html = null, $use_file = false)
+    {
+        if (file_exists(BASE . 'external/dompdf070/autoload.inc.php')) {
+            if (!file_exists(BASE . 'tmp/ttfontdata'))
+                expFile::makeDirectory('tmp/ttfontdata');
+            require_once(BASE . 'external/dompdf070/autoload.inc.php');
+            $this->pdf = new Dompdf\Dompdf();
+            $this->size = $paper_size;
+            $this->orient = $orientation;
+            $this->pdf->setPaper($this->size, $this->orient);
+            if (!empty($html)) {
+                if ($use_file) {
+                    $this->pdf->loadHtmlFile($html);
+                } else {
+                    $this->pdf->loadHtml($html);
+                }
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Render and Return PDF using various options.
+     *
+     * @param string $mode How to output (constants from this same class).
+     * @param string $file The PDF's filename (the usage depends on $mode.
+     *
+     * @return string|boolean Depending on $mode, this may be success (boolean) or PDF (string).
+     */
+    public function createpdf($mode, $file)
+    {
+        $this->pdf->render();
+        switch ($mode) {
+            case self::$PDF_ASSTRING:
+                return $this->pdf->output();
+                break;
+            case self::$PDF_EMBEDDED:
+                $this->pdf->stream($file, array('Attachment' => false));
+                break;
+            case self::$PDF_SAVEFILE:
+                return file_put_contents($file, $this->pdf->output());
+                break;
+            case self::$PDF_DOWNLOAD:
+                $this->pdf->stream($file, array('Attachment' => true));
+                break;
+            default:
+                $this->pdf->stream($file, array('Attachment' => HTMLTOPDF_OUTPUT));
+        }
+        return true;
+    }
+
+    /**
+     * Set orientation, use constants from this class.
+     * By default orientation is portrait.
+     *
+     * @param string $orientation orientation of paper
+     */
+    public function set_orientation($orientation)
+    {
+        $this->orient = $orientation;
+        $this->pdf->setPaper($this->size, $this->orient);
+    }
+
+    /**
+     * Set page/paper size.
+     * By default page size is A4.
+     *
+     * @param string $size Formal paper size (eg; A4, letter...)
+     */
+    public function set_page_size($size)
+    {
+        $this->size = $size;
+        $this->pdf->setPaper($this->size, $this->orient);
+    }
+
+    /**
+     * Set page/paper size.
+     * By default page size is A4.
+     *
+     * @param string $size        Formal paper size (eg; A4, letter...)
+     * @param string $orientation orientation of paper
+     */
+    public function set_paper($size, $orientation = 'portrait')
+    {
+        if (!empty($size)) {
+            $this->size = $size;
+        }
+        if (!empty($orientation)) {
+            $this->orient = $orientation;
+        }
+        $this->pdf->setPaper($this->size, $this->orient);
+    }
+
+    /**
+     * Set html content by string.
+     *
+     * @param string $html html content.
+     * @param null   $encoding
+     */
+    public function set_html($html, $encoding = null)
+    {
+        $this->pdf->loadHtml($html, $encoding);
+    }
+
+    /**
+     * Set html content by file.
+     *
+     * @param string $htmlfile the html file to use
+     */
+    public function set_html_file($htmlfile)
+    {
+        $this->pdf->loadHtmlFile($htmlfile);
+    }
+
+    /**
+     * Convert HTML to PDF.
+     */
+    public function render()
+    {
+        $this->pdf->render();
+    }
+
+    /**
+     * Streams the PDF to the client.
+     *
+     * @param string $filename the pdf file to output
+     * @param array  $options  options
+     *                         'compress' = > 1 or 0 - apply content stream compression, this is on (1) by default
+     *                         ◦   'Attachment' => 1 or 0 - if 1, force the browser to open a download dialog, on (1) by default
+     */
+    public function stream($filename, $options = null)
+    {
+        $this->pdf->stream($filename, $options);
+    }
+
+    /**
+     * Return PDF as a string.
+     *
+     * @param int $compress compress the output
+     *
+     * @return string
+     */
+    public function output($compress = null)
+    {
+        return $this->pdf->output($compress);
+    }
+
+    public function set_grayscale($mode)
+    {
+    }
+
+}
+
+/**
  * This is the class expMPDF
  * a wrapper for using mPDF
  *
