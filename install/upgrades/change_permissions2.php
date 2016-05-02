1cd192eed4e5198d6ef264339dbcdc678cfdc60e
dleffler <dleffler@hughes.net>
2016-05-02 16:50:00 -0400

    Regression fix the check permissions upgrade scripts were not properly avoiding the 'cgi-bin' folder

--- install/upgrades/change_permissions2.php
+++ install/upgrades/change_permissions2.php
@@ -63,7 +63,7 @@ class change_permissions2 extends upgradescript {
 
                 $filepath = $start_dir . '/' . $file;
                 if ( is_dir($filepath) ) {
-                    if ($filepath == 'cgi-bin')
+                    if ($file == 'cgi-bin')
                         break;
                     chmod($filepath, $dir_perms);
                     self::chmod_recursive($filepath);
