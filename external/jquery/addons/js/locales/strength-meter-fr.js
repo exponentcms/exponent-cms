2d40e5e4ae5c7cc1ae2e152eb0756dd49822bfdc
dleffler <dleffler@hughes.net>
2016-10-20 18:56:34 -0400

    adds some missing jquery plugin language files (not sure we even use these?)

---
+++ external/jquery/addons/js/locales/strength-meter-fr.js
@@ -0,0 +1,20 @@
+/**
+ * Strength Meter French translation.
+ * This file must be saved in UTF-8 encoding.
+ * @copyright &copy; Kartik Visweswaran, Krajee.com, 2015
+ */
+(function ($) {
+    "use strict";
+
+    $.fn.strengthLocales['fr'] = {
+        toggleTitle: 'Afficher / Masquer Mot de passe',
+        verdictTitles: {
+            0: 'Trop Court',
+            1: 'Très Faible',
+            2: 'Faible',
+            3: 'Bon',
+            4: 'Fort',
+            5: 'Très Fort'
+        }
+    };
+})(jQuery);
