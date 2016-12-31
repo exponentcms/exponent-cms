860f17ea4439c5f625db766eca641ba270fbe6aa
dleffler <dleffler@hughes.net>
2016-12-30 21:13:00 -0500

    remove old easypost v3.2.0 and swiftmailer v5.4.4

--- install/upgrades/remove_oldfiles.php
+++ install/upgrades/remove_oldfiles.php
@@ -620,6 +620,7 @@ class remove_oldfiles extends upgradescript {
 			'external/swiftmailer-5.4.1/',
 			'external/swiftmailer-5.4.2/',
             'external/swiftmailer-5.4.3/',
+            'external/swiftmailer-5.4.4/',
             'external/webshims-1.15.10/',
             'external/yui3/',
             'external/lissa/',
