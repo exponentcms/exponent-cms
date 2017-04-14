a186b00e9374d0381c2d354d2e7268b0af392d59
dleffler <dleffler@hughes.net>
2017-04-14 07:07:55 -0400

    update database manager (adminer) to v4.3.1

--- external/adminer/admin.php
+++ external/adminer/admin.php
@@ -148,7 +148,7 @@ function adminer_object() {
 }
 
 // include original Adminer or Adminer Editor
-include "./adminer-4.3.0-mysql.php";
+include "./adminer-4.3.1-mysql.php";
 
 if (SITE_WYSIWYG_EDITOR != 'tinymce') {
 ?>
