2a20f02d1a358ea9e03c1f12a296c33d9cd573ea
dleffler <dleffler@hughes.net>
2018-02-08 23:29:50 -0500

    update adminer db manager to v4.6.1

--- external/adminer/plugins/plugin.php
+++ external/adminer/plugins/plugin.php
@@ -97,7 +97,17 @@ class AdminerPlugin extends Adminer {
 		return $this->_applyPlugin(__FUNCTION__, $args);
 	}
 
+	function connectSsl() {
+		$args = func_get_args();
+		return $this->_applyPlugin(__FUNCTION__, $args);
+	}
+
 	function permanentLogin($create = false) {
+		$args = func_get_args();
+		return $this->_applyPlugin(__FUNCTION__, $args);
+	}
+
+	function serverName($server) {
 		$args = func_get_args();
 		return $this->_applyPlugin(__FUNCTION__, $args);
 	}
