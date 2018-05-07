37c19a3e0f02c3100c331470033678d17cf1ea38
Dave Leffler <dleffler@hughes.net>
2018-05-07 12:14:16 -0400

    update simpletheme configuration to use dropdowns instead of text input

--- themes/simpletheme/class.php
+++ themes/simpletheme/class.php
@@ -24,6 +24,43 @@ class simpletheme extends theme {
 	function name() { return "Simple Theme"; }
 	function author() { return "Phillip Ball - Online Innovative Creations"; }
 	function description() { return "A minimal, slick theme based on the <a href=\"http://yuilibrary.com/yui/docs/cssgrids/\" target=\"_blank\">YUI 3 Gridding System</a>"; }
+
+    function configureTheme() {
+   		//THEME COLORS
+        $theme_colors = array(
+            "orange"=>'Orange',
+            "blue"=>'Blue',
+            "red"=>'Red',
+            "green"=>'Green',
+            "purple"=>'Purple',
+            "black"=>'Black',
+            "yellow"=>'Yellow',
+            "pink"=>'Pink',
+            "grey"=>'Grey',
+            "magenta"=>'Magenta',
+        );
+   		//Button Sizes
+   		$button_sizes = array(
+   			"small"=>'Small',
+   			"medium"=>'Medium',
+   			"large"=>'Large',
+   		);
+
+   		$settings = expSettings::parseFile(BASE."themes/".$this->params['theme']."/config.php");
+   		$form = new form();
+   		$form->meta('controller','administration');
+   		$form->meta('action','update_theme');
+   		$form->meta('theme',$this->params['theme']);
+   		$form->register('btn_color',gt('Button Color').': ',new dropdowncontrol($settings['BTN_COLOR'],$theme_colors));
+   		$form->register('btn_size',gt('Button Size').': ',new dropdowncontrol($settings['BTN_SIZE'],$button_sizes));
+   //		$form->register(null,'',new htmlcontrol('<br>'));
+   		$form->register('submit','',new buttongroupcontrol(gt('Save'),'',gt('Cancel')));
+   		assign_to_template(array(
+               'name'=>$this->name().(!empty($this->params['sv'])?' '.$this->params['sv']:''),
+               'form_html'=>$form->tohtml()
+           ));
+   	}
+
 }
 
 ?>
--- themes/simpletheme/config_Fall.php
+++ themes/simpletheme/config_Fall.php
@@ -1,4 +1,5 @@
 <?php
 define('BTN_COLOR','red');
 define('BTN_SIZE','medium');
+define("THEME_CUSTOM_LANGUAGE",'0');
 ?>
