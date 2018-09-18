e2ef5d9182b66a74872e80b2b8cbd7e242b12ca0
dleffler <dleffler@hughes.net>
2018-09-17 21:59:20 -0400

    regression fix bs4 datetimecontrol time display

--- framework/core/forms/controls/bootstrap4/datetimecontrol.php
+++ framework/core/forms/controls/bootstrap4/datetimecontrol.php
@@ -87,29 +87,29 @@ class datetimecontrol extends formcontrol {
             if ($this->showtime)
                 $html .= '<label class="col-2 control-label">' . gt('Date') . ': </label>';
             $html .= expDateTime::monthsDropdown($name . "_month", $default_date['mon']);
-            $html .= '<input class="text form-control col-1" type="text" id="' . $name . '_day" name="' . $name . '_day" aria-label="' . gt('Day') . '" size="3" maxlength="2" value="' . $default_date['mday'] . '"';
+            $html .= '<input class="text form-control" type="text" id="' . $name . '_day" name="' . $name . '_day" aria-label="' . gt('Day') . '" size="3" maxlength="2" value="' . $default_date['mday'] . '"';
             if (!empty($this->readonly) || !empty($this->disabled)) $html .= ' disabled="disabled"';
             $html .= ' />';
-            $html .= '<input class="text form-control col-1" type="text" id="' . $name . '_year" name="' . $name . '_year" aria-label="' . gt('Month') . '" size="5" maxlength="4" value="' . $default_date['year'] . '"';
+            $html .= '<input class="text form-control" type="text" id="' . $name . '_year" name="' . $name . '_year" aria-label="' . gt('Month') . '" size="5" maxlength="4" value="' . $default_date['year'] . '"';
             if (!empty($this->readonly) || !empty($this->disabled)) $html .= ' disabled="disabled"';
             $html .= ' />';
             $html .= '</div>';
         }
         if ($this->showtime) {
             $framework = framework();
-            if ($framework != 'bootstrap' && $framework != 'bootstrap3') {
+            if ($framework !== 'bootstrap' && $framework !== 'bootstrap3') {
                 $html .= '<br /><label class="control-label label spacer"> </label>';
             }
             $html .= '<div class="datetime date time" style="display:inline-block;margin-left: 15px;">';
             if ($this->showdate)
                 $html .= '<label class="col-2 control-label">' . gt('Time') . ': </label>';
-            $html .= '<input class="text timebox form-control col-1" type="text" id="' . $name . '_hour" name="' . $name . '_hour" aria-label="' . gt('Hour') . '" size="3" maxlength="2" value="' . $hour . '"';
+            $html .= '<input class="text timebox form-control" type="text" id="' . $name . '_hour" name="' . $name . '_hour" aria-label="' . gt('Hour') . '" size="3" maxlength="2" value="' . $hour . '"';
             if (!empty($this->readonly) || !empty($this->disabled)) $html .= ' disabled="disabled"';
             $html .= ' />';
-            $html .= '<input class="text timebox form-control col-1" type="text" id="' . $name . '_minute" name="' . $name . '_minute" aria-label="' . gt('Minute') . '" size="3" maxlength="2" value="' . $minute . '"';
+            $html .= '<input class="text timebox form-control" type="text" id="' . $name . '_minute" name="' . $name . '_minute" aria-label="' . gt('Minute') . '" size="3" maxlength="2" value="' . $minute . '"';
             if (!empty($this->readonly) || !empty($this->disabled)) $html .= ' disabled="disabled"';
             $html .= ' />';
-            $html .= '<select class="select form-control col-1" id="' . $name . '_ampm" name="' . $name . '_ampm" size="1"';
+            $html .= '<select class="select form-control" id="' . $name . '_ampm" name="' . $name . '_ampm" size="1"';
             if (!empty($this->readonly) || !empty($this->disabled)) $html .= ' disabled="disabled"';
             $html .= '>';
             $html .= '<option value="am"' . ($default_date['hours'] < 12 ? " selected" : "") . '>am</option>';
@@ -133,10 +133,10 @@ class datetimecontrol extends formcontrol {
         if (isset($values[$name . "_month"]))
             $time = mktime(8, 0, 0, $values[$name . '_month'], $values[$name . '_day'], $values[$name . '_year']) - 8 * 3600;
         if (isset($values[$name . "_hour"])) {
-            if ($values[$name . '_hour'] == 12 && $values[$name . '_ampm'] == 'am') {
+            if ($values[$name . '_hour'] == 12 && $values[$name . '_ampm'] === 'am') {
                 // 12 am (right after midnight) is 0:xx
                 $values[$name . '_hour'] = 0;
-            } else if ($values[$name . '_hour'] != 12 && $values[$name . '_ampm'] == 'pm') {
+            } else if ($values[$name . '_hour'] != 12 && $values[$name . '_ampm'] === 'pm') {
                 // 1:00 pm to 11:59 pm shifts 12 hours
                 $values[$name . '_hour'] += 12;
             }
