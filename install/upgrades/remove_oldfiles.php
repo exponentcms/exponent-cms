6dd725190924e3fdafc7338f04d5d0d0136f7db0
dleffler <dleffler@hughes.net>
2020-02-14 12:08:51 -0500

    remove emailvalidator v2.1.11

--- install/upgrades/remove_oldfiles.php
+++ install/upgrades/remove_oldfiles.php
@@ -666,6 +666,7 @@ class remove_oldfiles extends upgradescript {
             'external/editors/images/',
             'external/EmailValidator-2.1.2/',
             'external/EmailValidator-2.1.4/',
+            'external/EmailValidator-2.1.11/',
             'external/facebook-php-sdk-3.2.2/',
             'external/fedex-php/',
             'external/fedex-phpv13/',
