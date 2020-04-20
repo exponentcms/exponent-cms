2a29cb494bddab60eff0dfd3f347a88808549622
dleffler <dleffler@hughes.net>
2020-04-19 22:20:49 -0400

    attempt to fix a import files issue

--- framework/modules/file/controllers/fileController.php
+++ framework/modules/file/controllers/fileController.php
@@ -939,11 +939,12 @@ class fileController extends expController {
                     break;
         	}
         } else {
-        	$basename = basename($_FILES['file']['name']);
+//        	$basename = basename($_FILES['file']['name']);
 
 //        	include_once(BASE.'external/Tar.php');  // fixme change to PharData
 //        	$tar = new Archive_Tar($_FILES['file']['tmp_name'],'gz');
-            $tar = new PharData($_FILES['file']['tmp_name']);
+            rename($_FILES['file']['tmp_name'], $_FILES['file']['tmp_name'] . '.tmp');
+            $tar = new PharData($_FILES['file']['tmp_name'] . '.tmp');
             $tar->decompress();  // uncompressed and creates .tar file
         	$dest_dir = BASE.'tmp/extensionuploads/'.uniqid('');
         	@mkdir($dest_dir, octdec(DIR_DEFAULT_MODE_STR + 0));
