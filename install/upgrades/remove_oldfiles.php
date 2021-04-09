ae12c7d6f5d6fae42d3c2259b90197eaa22f2ff7
dleffler <dleffler@hughes.net>
2021-04-09 10:13:05 -0400

    remove easypost 3.4.5

--- install/upgrades/remove_oldfiles.php
+++ install/upgrades/remove_oldfiles.php
@@ -668,6 +668,7 @@ class remove_oldfiles extends upgradescript {
             'external/easypost-php-3.4.0/',
             'external/easypost-php-3.4.1/',
             'external/easypost-php-3.4.4/',
+            'external/easypost-php-3.4.5/',
             'external/editors/connector/',
             'external/editors/FCKeditor/',
             'external/editors/ckeditor/images',
