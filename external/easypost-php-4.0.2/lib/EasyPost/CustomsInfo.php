ad1aa6fcfad4393a938b27a4aed0ce420578812f
dleffler <dleffler@hughes.net>
2021-10-29 09:55:58 -0400

    update easypost to v4.0.2

---
+++ external/easypost-php-4.0.2/lib/EasyPost/CustomsInfo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace EasyPost;
+
+class CustomsInfo extends EasypostResource
+{
+    /**
+     * retrieve a customs info
+     *
+     * @param string $id
+     * @param string $apiKey
+     * @return mixed
+     */
+    public static function retrieve($id, $apiKey = null)
+    {
+        return self::_retrieve(get_class(), $id, $apiKey);
+    }
+
+    /**
+     * retrieve all customs info
+     *
+     * @param mixed $params
+     * @param string $apiKey
+     * @return mixed
+     */
+    public static function all($params = null, $apiKey = null)
+    {
+        return self::_all(get_class(), $params, $apiKey);
+    }
+
+    /**
+     * create a customs info
+     *
+     * @param mixed  $params
+     * @param string $apiKey
+     * @return mixed
+     */
+    public static function create($params = null, $apiKey = null)
+    {
+        if (!isset($params['customs_info']) || !is_array($params['customs_info'])) {
+            $clone = $params;
+            unset($params);
+            $params['customs_info'] = $clone;
+        }
+
+        return self::_create(get_class(), $params, $apiKey);
+    }
+}
