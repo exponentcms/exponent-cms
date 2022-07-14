62a9de2f5e6bbc25755c6f0fa4aaef6cab611cbb
dleffler <dleffler@hughes.net>
2022-07-14 12:57:36 -0400

    add ecom currency symbol to javascript for future use

--- exponent.js.php
+++ exponent.js.php
@@ -39,6 +39,8 @@ EXPONENT.YUI2_VERSION = "<?php echo YUI2_VERSION; ?>";
 EXPONENT.YUI2_RELATIVE = "<?php echo YUI2_RELATIVE; ?>";
 EXPONENT.YUI2_URL = "<?php echo YUI2_URL; ?>";
 
+EXPONENT.ECOM_CURRENCY_SYMBOL = "<?php echo expCore::getCurrencySymbol(); ?>";
+
 // Helper function to kill console logging so it won't kill IE7-9
 //if (typeof console == "undefined" || typeof console.log == "undefined") {
 //   var console = { log: function() {} };
