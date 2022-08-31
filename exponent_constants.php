1997fc23b311738d742d74abd3d5f382a0bf7ad5
dleffler <dleffler@hughes.net>
2022-08-30 20:30:21 -0400

    update jquery to v3.6.1, jquery-migrate to v3.4.0, jquery-ui to 1.13.2

--- exponent_constants.php
+++ exponent_constants.php
@@ -532,9 +532,9 @@ if (!defined('YUI2_RELATIVE')) {
 if (!defined('JQUERY_RELATIVE')) {
     define('JQUERY_VERSION', '1.12.4');
     define('JQUERY2_VERSION', '2.2.4');
-    define('JQUERY3_VERSION', '3.6.0');
-    define('JQUERY3_MIGRATE_VERSION', '3.3.2');
-    define('JQUERYUI_VERSION', '1.13.0');
+    define('JQUERY3_VERSION', '3.6.1');
+    define('JQUERY3_MIGRATE_VERSION', '3.4.0');
+    define('JQUERYUI_VERSION', '1.13.2');
     define('JQUERY_RELATIVE', PATH_RELATIVE . 'external/jquery/');
     define('JQUERY_PATH', BASE . 'external/jquery/');
     define('JQUERY_URL', URL_FULL . 'external/jquery/');
