691f4c83660aa2bcc162c74bcc0796d55d63633c
dleffler <dleffler@hughes.net>
2024-03-28 16:33:03 -0400

    revert to smarty v4.4.1 since v4.5.1 spits out deprecation warnings

--- exponent_constants.php
+++ exponent_constants.php
@@ -616,7 +616,7 @@ if (!defined('SMARTY_PATH')) {
     if (version_compare(PHP_VERSION, '7.1.0', 'lt')) {
         define('SMARTY_VERSION', '3.1.27');
     } else {
-        define('SMARTY_VERSION', '4.5.1');  //note smarty v3.1.28+ won't work properly with php < v7.1+
+        define('SMARTY_VERSION', '4.4.1');  //note smarty v3.1.28+ won't work properly with php < v7.1+
     }
     define('SMARTY_PATH', BASE . 'external/smarty-' . SMARTY_VERSION . '/libs/');
 //    define('SMARTY_DEVELOPMENT', false);
