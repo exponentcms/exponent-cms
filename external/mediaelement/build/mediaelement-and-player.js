b5310706d457684368d35e1d7643eb62fb626709
dleffler <dleffler@hughes.net>
2024-04-29 11:11:38 -0400

    update mediaelement.js to v7.0.5

--- external/mediaelement/build/mediaelement-and-player.js
+++ external/mediaelement/build/mediaelement-and-player.js
@@ -1116,7 +1116,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var mejs = {};
 
-mejs.version = '7.0.3';
+mejs.version = '7.0.5';
 
 mejs.html5media = {
 	properties: ['volume', 'src', 'currentTime', 'muted', 'duration', 'paused', 'ended', 'buffered', 'error', 'networkState', 'readyState', 'seeking', 'seekable', 'currentSrc', 'preload', 'bufferedBytes', 'bufferedTime', 'initialTime', 'startOffsetTime', 'defaultPlaybackRate', 'playbackRate', 'played', 'autoplay', 'loop', 'controls'],
@@ -2437,13 +2437,12 @@ Object.assign(_player2.default.prototype, {
 
     if (t.options.toggleCaptionsButtonWhenOnlyOne && subtitles.length === 1) {
       player.captionsButton.classList.add(t.options.classPrefix + 'captions-button-toggle');
-      player.captionsButton.addEventListener('click', function (e) {
+      player.captionsButton.addEventListener('click', function () {
         var trackId = 'none';
         if (player.selectedTrack === null) {
           trackId = player.getSubtitles()[0].trackId;
         }
-        var keyboard = e.keyCode || e.which;
-        player.setTrack(trackId, typeof keyboard !== 'undefined');
+        player.setTrack(trackId);
       });
     } else {
       var labels = player.captionsButton.querySelectorAll('.' + t.options.classPrefix + 'captions-selector-label'),
@@ -2467,9 +2466,8 @@ Object.assign(_player2.default.prototype, {
 
       for (var _i3 = 0; _i3 < captions.length; _i3++) {
         captions[_i3].addEventListener('click', function (e) {
-          var keyboard = e.keyCode || e.which;
           if (!e.target.disabled) {
-            player.setTrack(this.value, typeof keyboard !== 'undefined');
+            player.setTrack(this.value);
           }
         });
       }
@@ -2676,7 +2674,7 @@ Object.assign(_player2.default.prototype, {
       }
     }
   },
-  setTrack: function setTrack(trackId, setByKeyboard) {
+  setTrack: function setTrack(trackId) {
     var t = this,
         radios = t.captionsButton.querySelectorAll('input[type="radio"]'),
         captions = t.captionsButton.querySelectorAll('.' + t.options.classPrefix + 'captions-selected'),
@@ -2716,12 +2714,6 @@ Object.assign(_player2.default.prototype, {
     var event = (0, _general.createEvent)('captionschange', t.media);
     event.detail.caption = t.selectedTrack;
     t.media.dispatchEvent(event);
-
-    if (!setByKeyboard) {
-      setTimeout(function () {
-        t.getElement(t.container).focus();
-      }, 500);
-    }
   },
   hideAllTracks: function hideAllTracks() {
     if (this.node.textTracks) {
@@ -6795,24 +6787,7 @@ var IS_FIREFOX = exports.IS_FIREFOX = /firefox/i.test(UA);
 var IS_SAFARI = exports.IS_SAFARI = /safari/i.test(UA) && !IS_CHROME;
 var IS_STOCK_ANDROID = exports.IS_STOCK_ANDROID = /^mozilla\/\d+\.\d+\s\(linux;\su;/i.test(UA);
 var HAS_MSE = exports.HAS_MSE = 'MediaSource' in _window2.default;
-var SUPPORT_POINTER_EVENTS = exports.SUPPORT_POINTER_EVENTS = function () {
-	var element = _document2.default.createElement('x'),
-	    documentElement = _document2.default.documentElement,
-	    getComputedStyle = _window2.default.getComputedStyle;
-
-	if (!('pointerEvents' in element.style)) {
-		return false;
-	}
-
-	element.style.pointerEvents = 'auto';
-	element.style.pointerEvents = 'x';
-	documentElement.appendChild(element);
-	var supports = getComputedStyle && (getComputedStyle(element, '') || {}).pointerEvents === 'auto';
-	element.remove();
-	return !!supports;
-}();
-
-var SUPPORT_PASSIVE_EVENT = exports.SUPPORT_PASSIVE_EVENT = function () {
+var SUPPORT_POINTER_EVENTS = exports.SUPPORT_POINTER_EVENTS = true;var SUPPORT_PASSIVE_EVENT = exports.SUPPORT_PASSIVE_EVENT = function () {
 	var supportsPassive = false;
 	try {
 		var opts = Object.defineProperty({}, 'passive', {
