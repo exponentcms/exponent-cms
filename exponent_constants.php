f2b94ee11558744906d0da6a7f497bcc5438c5fc
dleffler <dleffler@hughes.net>
2025-05-09 08:53:25 -0400

    update bootstrap to v5.3.6

--- exponent_constants.php
+++ exponent_constants.php
@@ -590,7 +590,7 @@ if (!defined('BS4_SCRIPT')) {
     define('BS4_SCRIPT', '<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>');
 } // cdn Bootstrap v4.x script
 if (!defined('BS5_SCRIPT')) {
-    define('BS5_SCRIPT', '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>');
+    define('BS5_SCRIPT', '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>');
 } // cdn Bootstrap v5.x script
 /**
  * Font-Awesome CDN links
