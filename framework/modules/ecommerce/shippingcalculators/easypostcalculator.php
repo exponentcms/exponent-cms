da1975cae699f4a482a15684d2a8bac4140bf9b7
dleffler <dleffler@hughes.net>
2025-06-25 07:38:25 -0400

    update use of easypost lib

--- framework/modules/ecommerce/shippingcalculators/easypostcalculator.php
+++ framework/modules/ecommerce/shippingcalculators/easypostcalculator.php
@@ -23,7 +23,7 @@
  */
 class easypostcalculator extends shippingcalculator
 {
-    private static $version = '7.4.2';  // library version
+    private static $version = '8.2.0';  // library version
     /*
      * Returns the name of the shipping calculator, for use in the Shipping Administration Module
      */
