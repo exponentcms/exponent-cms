29f92f4f1388def65bbafd394d1274e0d1cedd65
dleffler <dleffler@hughes.net>
2026-04-10 17:25:10 -0400

    remove old easypost library

--- external/easypost-php-8.4.0/lib/EasyPost/Exception/Api/TimeoutException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace EasyPost\Exception\Api;
-
-class TimeoutException extends ApiException
-{
-}
--- external/easypost-php-8.4.0/lib/EasyPost/Exception/General/SignatureVerificationException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace EasyPost\Exception\General;
-
-class SignatureVerificationException extends EasyPostException
-{
-}
